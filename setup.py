9001eb2eff3be0fee1f4804c89ecb51705fc76a9
Matt Davis <jiffyclub@gmail.com>
2014-06-18 16:54:34 -0700

    Initial commit of ActivitySim project skeleton.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name='activitysim',
+    version='0.1dev',
+    description='Travel modeling',
+    author='Synthicity',
+    author_email='pwaddell@synthicity.com',
+    license='AGPL',
+    url='https://github.com/synthicity/activitysim',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU Affero General Public License v3'
+    ],
+    packages=find_packages(exclude=['*.tests']),
+    install_requires=[
+        'numpy>=1.8.0',
+        'pandas>=0.13.1',
+    ]
+)

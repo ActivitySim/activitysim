64db23b57efe25a1354b499d766c0a70dd4701e8
Matt Davis <jiffyclub@gmail.com>
2014-12-11 22:03:28 -0800

    Add Apache v2 license and license notices to OMX files

--- activitysim/omx/omxfile.py
+++ activitysim/omx/omxfile.py
@@ -1,3 +1,8 @@
+# Licensed under the Apache License, v2.0
+# See activitysim/omx/LICENSE.txt
+# Modified from the original OMX library available at
+# https://github.com/osPlanning/omx/tree/dev/api/python/omx
+
 import numpy as np
 import tables
 

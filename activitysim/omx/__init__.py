4d443325178c9cbf53305003909132748fb05793
Matt Davis <jiffyclub@gmail.com>
2015-02-10 12:56:27 -0800

    Remove OMX module
    We'll use the seperate openmatrix library instead.

--- activitysim/omx/__init__.py
+++
@@ -1,7 +0,0 @@
-# Licensed under the Apache License, v2.0
-# See activitysim/omx/LICENSE.txt
-# Modified from the original OMX library available at
-# https://github.com/osPlanning/omx/tree/dev/api/python/omx
-
-from .omxfile import OMXFile, open_omxfile, OMX_VERSION
-from .exceptions import ShapeError

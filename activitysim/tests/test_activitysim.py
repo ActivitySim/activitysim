e01c0b2a958c22e2522180c1f0c70e76ed03eac0
Matt Davis <jiffyclub@gmail.com>
2015-02-25 19:49:09 -0800

    function for evaluating variables from specs

--- activitysim/tests/test_activitysim.py
+++ activitysim/tests/test_activitysim.py
@@ -1,13 +1,38 @@
 import os.path
 
 import numpy.testing as npt
+import pandas as pd
+import pandas.util.testing as pdt
+import pytest
 
 from .. import activitysim as asim
 
 
-def test_read_model_spec():
+@pytest.fixture(scope='module')
+def spec_name():
+    return os.path.join(os.path.dirname(__file__), 'data', 'sample_spec.csv')
+
+
+@pytest.fixture(scope='module')
+def data_name():
+    return os.path.join(os.path.dirname(__file__), 'data', 'data.csv')
+
+
+@pytest.fixture(scope='module')
+def spec(spec_name):
+    return asim.read_model_spec(
+        spec_name,
+        description_name='description', expression_name='expression')
+
+
+@pytest.fixture(scope='module')
+def data(data_name):
+    return pd.read_csv(data_name)
+
+
+def test_read_model_spec(spec_name):
     spec = asim.read_model_spec(
-        os.path.join(os.path.dirname(__file__), 'data', 'sample_spec.csv'),
+        spec_name,
         description_name='description', expression_name='expression')
 
     assert len(spec) == 3
@@ -25,3 +50,16 @@ def test_identity_matrix():
     npt.assert_array_equal(
         i.as_matrix(),
         [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+
+def test_eval_variables(spec, data):
+    result = asim.eval_variables(spec.index, data)
+
+    pdt.assert_frame_equal(
+        result,
+        pd.DataFrame([
+            [True, False, 4],
+            [False, True, 4],
+            [False, True, 5]],
+            index=data.index, columns=spec.index),
+        check_names=False)

7cc53672b49d1b409282b35ebc0843716b60eabc
Fletcher Foti <fscottfoti@gmail.com>
2015-04-02 16:40:50 -0700

    has all people, a few edits, and seems to be running

--- activitysim/defaults/models/cdap.py
+++ activitysim/defaults/models/cdap.py
@@ -42,7 +42,6 @@ def cdap_final_rules(configs_dir):
 
 @sim.injectable()
 def cdap_all_people(configs_dir):
-    return None
     f = os.path.join(configs_dir, 'configs', "cdap_all_people.csv")
     return asim.read_model_spec(f).fillna(0)
 
@@ -51,8 +50,6 @@ def cdap_all_people(configs_dir):
 def cdap_simulate(set_random_seed, persons_merged,
                   cdap_1_person_spec, cdap_2_person_spec, cdap_3_person_spec,
                   cdap_final_rules, cdap_all_people):
-
-    print cdap_3_person_spec
 
     choices = cdap.run_cdap(persons_merged.to_frame(),
                             "household_id",

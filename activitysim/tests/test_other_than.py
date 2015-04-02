9439a2482532b07017ce432e674025b080188d72
Matt Davis <jiffyclub@gmail.com>
2015-04-01 22:14:59 -0700

    use custom index for test just in case

--- activitysim/tests/test_other_than.py
+++ activitysim/tests/test_other_than.py
@@ -10,12 +10,14 @@ from ..activitysim import other_than
 def people():
     return pd.DataFrame({
         'household': [1, 2, 2, 3, 3, 3, 4, 4, 4, 4],
-        'ptype':     [1, 2, 1, 3, 1, 2, 3, 2, 2, 1]})
+        'ptype':     [1, 2, 1, 3, 1, 2, 3, 2, 2, 1]},
+        index=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
 
 
 def test_other_than(people):
     expected = pd.Series(
-        [False, False, True, True, True, False, True, True, True, True])
+        [False, False, True, True, True, False, True, True, True, True],
+        index=people.index)
 
     bools = people['ptype'] == 2
     others = other_than(people['household'], bools)

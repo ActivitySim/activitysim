405a94bcddcc857721e99936ba1aaaa005b95dd6
Fletcher Foti <fscottfoti@gmail.com>
2015-04-06 15:07:22 -0700

    large advance on mandatory scheduling
    Only a few "windowing" variables are left unimplemented.
    
    This implements all of the mandatory scheduling spec otherwise, as well as vectorizing the trip scheduling (doing the 1st trips, 2nd trips, etc for each person) and also fills in the start_previous and end_prevoius variables for the previous trip for each person.  This functionality is now a separate utility function in the util directory and has doc strings and unit tests now.

--- activitysim/defaults/models/mandatory_scheduling.py
+++ activitysim/defaults/models/mandatory_scheduling.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import urbansim.sim.simulation as sim
 from activitysim import activitysim as asim
-from non_mandatory_scheduling import vectorize_tour_schedules
+from .util.vectorize_tour_scheduling import vectorize_tour_scheduling
 
 """
 This model predicts the departure time and duration of each activity for
@@ -39,13 +39,12 @@ def mandatory_scheduling(set_random_seed,
                          tdd_mandatory_spec):
 
     tours = mandatory_tours_merged.to_frame()
+    alts = tdd_alts.to_frame()
+    spec = tdd_mandatory_spec.to_frame()
 
     print "Running %d mandatory tour scheduling choices" % len(tours)
 
-    spec = tdd_mandatory_spec.to_frame()
-    alts = tdd_alts.to_frame()
-
-    choices = vectorize_tour_schedules(tours, alts, spec)
+    choices = vectorize_tour_scheduling(tours, alts, spec)
 
     print "Choices:\n", choices.describe()
 
---
+++ activitysim/defaults/models/util/test/test_vectorize_tour_scheduling.py
@@ -0,0 +1,58 @@
+# ActivitySim
+# Copyright (C) 2014-2015 Synthicity, LLC
+# See full license in LICENSE.txt.
+
+
+import pytest
+import pandas as pd
+import numpy as np
+import pandas.util.testing as pdt
+from ..vectorize_tour_scheduling import get_previous_tour_by_tourid, \
+    vectorize_tour_scheduling
+
+
+def test_vts():
+
+    np.random.seed(0)
+
+    alts = pd.DataFrame({
+        "start": [1, 2, 3],
+        "end": [4, 5, 6],
+    }, index=[10, 20, 30])
+
+    current_tour_person_ids = pd.Series(['b', 'c'],
+                                        index=['d', 'e'])
+
+    previous_tour_by_personid = pd.Series([20, 20, 10],
+                                          index=['a', 'b', 'c'])
+
+    prev_tour_attrs = get_previous_tour_by_tourid(current_tour_person_ids,
+                                                  previous_tour_by_personid,
+                                                  alts)
+
+    pdt.assert_series_equal(
+        prev_tour_attrs.start_previous,
+        pd.Series([2, 1], index=['d', 'e']))
+
+    pdt.assert_series_equal(
+        prev_tour_attrs.end_previous,
+        pd.Series([5, 4], index=['d', 'e']))
+
+    tours = pd.DataFrame({
+        "person_id": [1, 1, 2, 3, 3],
+        "income": [20, 20, 30, 25, 25]
+    })
+
+    spec = pd.DataFrame({"Coefficient": [1.2]},
+                        index=["income"])
+    spec.index.name = "Expression"
+
+    choices = vectorize_tour_scheduling(tours, alts, spec)
+
+    # there's no real logic here - this is just what came out of the monte carlo
+    # note that the result comes out ordered by the nth trips and not ordered
+    # by the trip index.  shrug?
+    pdt.assert_series_equal(
+        choices,
+        pd.Series([20, 20, 30, 30, 20],
+                  index=[0, 2, 3, 1, 4]))

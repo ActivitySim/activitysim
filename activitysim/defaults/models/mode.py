ac5ef2ef37995abd49f096fa886c5465966747e5
Fletcher Foti <fscottfoti@gmail.com>
2015-04-24 15:36:05 -0700

    unit test and document the utilities used in mode choice

--- activitysim/defaults/models/mode.py
+++ activitysim/defaults/models/mode.py
@@ -1,12 +1,10 @@
 import os
-import copy
 import yaml
-import string
 import pandas as pd
-import numpy as np
 import urbansim.sim.simulation as sim
 from activitysim import activitysim as asim
 from activitysim import skim as askim
+from .util.mode import mode_choice_spec
 
 """
 Mode choice is run for all tours to determine the transportation mode that
@@ -38,114 +36,20 @@ def mode_choice_coeffs(configs_dir):
         return pd.read_csv(f, index_col='Expression')
 
 
-# FIXME move into activitysim as a utility function
-def evaluate_expression_list(expressions, locals_d):
-    """
-    Evaluate a list of expressions - each one can depend on the one before it
-
-    Parameters
-    ----------
-    expressions : Series
-        indexes are names and values are expressions
-    locals_d : dict
-        will be passed directly to eval
-
-    Returns
-    -------
-    expressions : Series
-        index is the same as above, but values are now confirmed to be floats
-    """
-    d = {}
-    # this could be a simple expression except that the dictionary
-    # is accumulating expressions - i.e. they're not all independent
-    # and must be evaluated in order
-    for k, v in expressions.iteritems():
-        # make sure it can be converted to a float
-        d[k] = float(eval(str(v), copy.copy(d), locals_d))
-    return pd.Series(d)
-
-
-def pre_process_expressions(expressions, variable_templates):
-    return [eval(e[1:], variable_templates) if e.startswith('$') else e for
-            e in expressions]
-
-
-def get_segment_and_unstack(spec, segment):
-    return spec[segment].unstack().\
-        reset_index(level="Rowid", drop=True).fillna(0)
-
-
-def expand_alternatives(df):
-    # alternatives are kept as a comma separated list.  At this stage we need
-    # need to split them up so that there is only one alternative per row, and
-    # where an expression is shared among alternatives, that row is copied
-    # with each alternative alternative value (pun intended) substituted for
-    # the alternative value for each row
-
-    # first split up the alts using string.split
-    alts = [string.split(s, ",") for s in df.reset_index()['Alternative']]
-
-    # this is the number of alternatives in each row
-    len_alts = [len(x) for x in alts]
-
-    # this repeats the locs for the number of alternatives in each row
-    ilocs = np.repeat(np.arange(len(df)), len_alts)
-
-    # grab the rows the right number of times (after setting a rowid)
-    df['Rowid'] = np.arange(len(df))
-    df = df.iloc[ilocs]
-
-    # now concat all the lists
-    new_alts = sum(alts, [])
-
-    df.reset_index("Alternative", inplace=True)
-    df["Alternative"] = new_alts
-    # rowid needs to bet set here - we're going to unstack this and we need
-    # a unique identifier to keep track of the rows during the unstack
-    df = df.set_index(['Rowid', 'Alternative'], append=True)
-
-    return df
-
-
 @sim.injectable()
 def mode_choice_spec(mode_choice_spec_df, mode_choice_coeffs,
                      mode_choice_settings):
-
-    # ok we have read in the spec - we need to do several things to reformat it
-    # to the same style spec that all the other models have
-
-    constants = mode_choice_settings['CONSTANTS']
-    templates = mode_choice_settings['VARIABLE_TEMPLATES']
-    df = mode_choice_spec_df
-
-    # the expressions themselves can be prepended with a "$" in order to use
-    # model templates that are shared by several different expressions
-    df.index = pre_process_expressions(df.index, templates)
-
-    df = df.set_index('Alternative', append=True)
-
-    # for each segment - e.g. eatout vs social vs work vs ...
-    for col in df.columns:
-
-        # first the coeffs come as expressions that refer to previous cells
-        # as well as constants that come from the settings file
-        mode_choice_coeffs[col] = evaluate_expression_list(
-            mode_choice_coeffs[col],
-            locals_d=constants)
-
-        # then use the coeffs we just evaluated within the spec (they occur
-        # multiple times in the spec which is why they get stored uniquely
-        # in a different file
-        df[col] = evaluate_expression_list(
-            df[col],
-            mode_choice_coeffs[col].to_dict())
-
-    df = expand_alternatives(df)
-
-    return df
+    return mode_choice_spec(mode_choice_spec_df, mode_choice_coeffs,
+                            mode_choice_settings)
 
 
 def _mode_choice_simulate(tours, skims, spec, additional_constants):
+    """
+    This is a utility to run a mode choice model for each segment (usually
+    segments are trip purposes).  Pass in the tours that need a mode,
+    the Skim object, the spec to evaluate with, and any additional expressions
+    you want to use in the evaluation of variables.
+    """
 
     # FIXME - this is only really going for the workplace trip
     in_skims = askim.Skims3D(skims.set_keys("TAZ", "workplace_taz"),
@@ -172,6 +76,22 @@ def _mode_choice_simulate(tours, skims, spec, additional_constants):
     return choices
 
 
+def get_segment_and_unstack(spec, segment):
+    """
+    This does what it says.  Take the spec, get the column from the spec for
+    the given segment, and unstack.  It is assumed that the last column of
+    the multiindex is alternatives so when you do this unstacking,
+    each alternative is in a column (which is the format this as used for the
+    simple_simulate call.  The weird nuance here is the "Rowid" column -
+    since many expressions are repeated (e.g. many are just "1") a Rowid
+    column is necessary to identify which alternatives are actually part of
+    which original row - otherwise the unstack is incorrect (i.e. the index
+    is not unique)
+    """
+    return spec[segment].unstack().\
+        reset_index(level="Rowid", drop=True).fillna(0)
+
+
 @sim.model()
 def mode_choice_simulate(tours_merged,
                          mode_choice_spec,
@@ -183,6 +103,7 @@ def mode_choice_simulate(tours_merged,
     mode_choice_spec = mode_choice_spec
     print mode_choice_spec.eatout
 
+    # FIXME this only runs eatout
     choices = _mode_choice_simulate(
         tours[tours.tour_type == "eatout"],
         skims,
---
+++ activitysim/defaults/models/util/test/test_mode.py
@@ -0,0 +1,102 @@
+# ActivitySim
+# Copyright (C) 2014-2015 Synthicity, LLC
+# See full license in LICENSE.txt.
+
+import pandas as pd
+import pandas.util.testing as pdt
+from ..mode import pre_process_expressions, evaluate_expression_list, \
+    expand_alternatives, mode_choice_spec
+
+
+def test_ppe():
+    ret = pre_process_expressions(
+        ['1', '$expr.format(var="bar")'],
+        {'expr': '@foo * {var}'}
+    )
+    assert ret[0] == '1'
+    assert ret[1] == '@foo * bar'
+
+
+def test_eel():
+    ret = evaluate_expression_list(
+        pd.Series(
+            ['.7', 'ivt * .7 * COST'],
+            index=['ivt', 'ivt_lr']
+        ),
+        {'COST': 2.0}
+    )
+    pdt.assert_series_equal(
+        ret,
+        pd.Series(
+            [.7, .98],
+            index=['ivt', 'ivt_lr']
+        )
+    )
+
+
+def test_ea():
+    df = pd.DataFrame({
+        "Alternative": ["One", "One,Two"],
+        "Other column": [1, 2]
+    }).set_index("Alternative")
+
+    df = expand_alternatives(df)
+
+    pdt.assert_series_equal(
+        df.reset_index().Alternative,
+        pd.Series(
+            ["One", "One", "Two"], index=[0, 1, 2]))
+
+    pdt.assert_series_equal(
+        df.reset_index().Rowid,
+        pd.Series(
+            [0, 1, 1], index=[0, 1, 2]))
+
+    pdt.assert_series_equal(
+        df.reset_index()["Other column"],
+        pd.Series(
+            [1, 2, 2], index=[0, 1, 2]))
+
+
+def test_mode_choice_spec():
+
+    spec = pd.DataFrame({
+        "Alternative": ["One", "One,Two"],
+        "Expression": ['1', '$expr.format(var="bar")'],
+        "Work": ['ivt', 'ivt_lr']
+    }).set_index(["Expression"])
+
+    coeffs = pd.DataFrame({
+        "Work": ['.7', 'ivt * .7 * COST']
+    }, index=['ivt', 'ivt_lr'])
+
+    settings = {
+        "CONSTANTS": {
+            "COST": 2.0
+        },
+        "VARIABLE_TEMPLATES": {
+            'expr': '@foo * {var}'
+        }
+    }
+
+    df = mode_choice_spec(spec, coeffs, settings)
+
+    pdt.assert_series_equal(
+        df.reset_index().Alternative,
+        pd.Series(
+            ["One", "One", "Two"], index=[0, 1, 2]))
+
+    pdt.assert_series_equal(
+        df.reset_index().Rowid,
+        pd.Series(
+            [0, 1, 1], index=[0, 1, 2]))
+
+    pdt.assert_series_equal(
+        df.reset_index()["Work"],
+        pd.Series(
+            [.7, .98, .98], index=[0, 1, 2]))
+
+    pdt.assert_series_equal(
+        df.reset_index()["Expression"],
+        pd.Series(
+            ["1", "@foo * bar", "@foo * bar"], index=[0, 1, 2]))

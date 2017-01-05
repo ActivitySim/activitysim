e572d0282953d67c93b27223d67d688c2d10d708
bstabler <benstabler@yahoo.com>
2017-01-05 15:17:22 -0800

    update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as file:
 
 setup(
     name='activitysim',
-    version='0.2.dev',
+    version='0.2.dev1',
     description='Activity-Based Travel Modeling',
     author='contributing authors',
     author_email='ben.stabler@rsginc.com',

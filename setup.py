3165d0a5815121a6a181e05f6e731322ce6f5090
Jeff Doyle <toliwaga@gmail.com>
2018-02-09 13:39:26 -0500

    activitysim version number to 0.5.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as file:
 
 setup(
     name='activitysim',
-    version='0.5.1',
+    version='0.5.2',
     description='Activity-Based Travel Modeling',
     author='contributing authors',
     author_email='ben.stabler@rsginc.com',

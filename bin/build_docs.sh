88b5d4f1e18e3bdcc3024c410d0561a9fcdc63d4
Jeff Doyle <toliwaga@gmail.com>
2018-02-17 17:21:17 -0500

    install build_docs sphinx_rtd_theme dependncy in .travis.yml with sphinx and numpydoc instad of in script

--- bin/build_docs.sh
+++ bin/build_docs.sh
@@ -33,9 +33,10 @@ if [ "$TRAVIS_REPO_SLUG" == "ActivitySim/activitysim" ] && \
         [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
         [ "$ACTUAL_TRAVIS_JOB_NUMBER" == "1" ]; then
 
-        echo "Installing dependencies"
-        conda install --yes --quiet sphinx numpydoc
-        pip install sphinx_rtd_theme
+        # assume these are installed already
+        # echo "Installing dependencies"
+        # conda install --yes --quiet sphinx numpydoc
+        # pip install sphinx_rtd_theme
 
         echo "Building docs"
         cd docs

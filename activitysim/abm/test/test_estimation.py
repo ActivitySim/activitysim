acd11a44b7c2411b1aa697650919ce442e576f6b
Jeff Newman <jeffnewman@camsys.com>
2021-01-24 16:46:54 -0600

    regression testing for larch re-estimated models

--- activitysim/abm/test/test_estimation.py
+++ activitysim/abm/test/test_estimation.py
@@ -33,54 +33,97 @@ def est_data():
             '-d', 'data_sf',
         ], capture_output=True)
 
-    yield
+    yield os.getcwd()
 
     os.chdir(cwd)
-    if not retain_test_data:
-        os.remove("_test_est")
+    # if not retain_test_data:
+    #     os.remove("_test_est")
 
 
 def _regression_check(dataframe_regression, df):
     dataframe_regression.check(df.select_dtypes('number').clip(-9e9,9e9))
 
 
-def test_auto_ownership(est_data, dataframe_regression):
+def test_auto_ownership(est_data, data_regression, dataframe_regression):
     from larch.util.activitysim.auto_ownership import auto_ownership_model
     m = auto_ownership_model()
     m.load_data()
-    assert m.loglike() == approx(-1745.4932922174953)
+    loglike_prior = m.loglike()
     r = m.maximize_loglike()
-    assert r.loglike == approx(-1724.943397053043)
+    data_regression.check({
+        'loglike_prior': loglike_prior,
+        'loglike_converge': r.loglike,
+    })
     _regression_check(dataframe_regression, m.pf)
 
 
-def test_workplace_location(est_data, dataframe_regression):
+def test_workplace_location(est_data, data_regression, dataframe_regression):
     from larch.util.activitysim.location_choice import location_choice_model
     m = location_choice_model(model_selector='workplace')
     m.load_data()
-    assert m.loglike() == approx(-13412.269435487342)
+    loglike_prior = m.loglike()
     r = m.maximize_loglike(method='SLSQP')
-    assert r.loglike == approx(-12624.992531894903)
+    data_regression.check({
+        'loglike_prior': loglike_prior,
+        'loglike_converge': r.loglike,
+    })
     _regression_check(dataframe_regression, m.pf)
 
 
-def test_school_location(est_data, dataframe_regression):
+def test_school_location(est_data, data_regression, dataframe_regression):
     from larch.util.activitysim.location_choice import location_choice_model
     m = location_choice_model(model_selector='school')
     m.load_data()
-    assert m.loglike() == approx(-3729.5884682769906)
+    loglike_prior = m.loglike()
     r = m.maximize_loglike(method='BHHH')
-    assert r.loglike == approx(-3097.6915329312333)
+    data_regression.check({
+        'loglike_prior': loglike_prior,
+        'loglike_converge': r.loglike,
+    })
     _regression_check(dataframe_regression, m.pf)
 
 
-def test_tour_mode_choice(est_data, dataframe_regression):
+def test_cdap_model(est_data, data_regression, dataframe_regression):
+    from larch.util.activitysim.cdap import cdap_model
+    m = cdap_model()
+    m.load_data()
+    loglike_prior = m.loglike()
+    r = m.maximize_loglike(method='SLSQP', options={'maxiter': 1000})
+    data_regression.check({
+        'loglike_prior': loglike_prior,
+        'loglike_converge': r.loglike,
+    })
+    _regression_check(dataframe_regression, m.pf)
+
+
+def test_tour_mode_choice(est_data, data_regression, dataframe_regression):
     from larch.util.activitysim.tour_mode_choice import tour_mode_choice_model
     m = tour_mode_choice_model()
     m.load_data()
     m.doctor(repair_ch_av='-')
-    assert m.loglike() == approx(-6069.594743230453)
+    loglike_prior = m.loglike()
     r = m.maximize_loglike(method='SLSQP', options={'maxiter': 1000})
-    assert r.loglike == approx(-5877.503582602247)
+    data_regression.check({
+        'loglike_prior': loglike_prior,
+        'loglike_converge': r.loglike,
+    })
     _regression_check(dataframe_regression, m.pf)
 
+
+def test_nonmand_tour_freq(est_data, data_regression, dataframe_regression):
+    from larch.util.activitysim.nonmand_tour_freq import nonmand_tour_freq_model
+    m = nonmand_tour_freq_model()
+    loglike_prior = {}
+    for segment_name in m:
+        m[segment_name].load_data()
+        m[segment_name].doctor(repair_ch_av='-')
+        loglike_prior[segment_name] = m[segment_name].loglike()
+    r = {}
+    for segment_name in m:
+        r[segment_name] = m[segment_name].maximize_loglike(method='SLSQP', options={'maxiter': 1000})
+    loglike_converge = {k:x.loglike for k,x in r.items()}
+    data_regression.check({
+        'loglike_prior': loglike_prior,
+        'loglike_converge': loglike_converge,
+    })
+    _regression_check(dataframe_regression, pd.concat([x.pf for x in m.values()]))

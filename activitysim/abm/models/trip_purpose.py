dc01d763556afc5a521e85a2522929441f8fb9c4
Max <gardner.max@gmail.com>
2021-03-04 19:19:32 -0800

    xborder trip purpose now runs

--- activitysim/abm/models/trip_purpose.py
+++ activitysim/abm/models/trip_purpose.py
@@ -16,16 +16,13 @@ from activitysim.core import expressions
 logger = logging.getLogger(__name__)
 
 
-PROBS_JOIN_COLUMNS = ['primary_purpose', 'outbound', 'person_type']
-
-
 def trip_purpose_probs():
     f = config.config_file_path('trip_purpose_probs.csv')
     df = pd.read_csv(f, comment='#')
     return df
 
 
-def trip_purpose_calc_row_size(choosers, spec, trace_label):
+def trip_purpose_calc_row_size(choosers, spec, probs_join_cols, trace_label):
     """
     rows_per_chunk calculator for trip_purpose
     """
@@ -33,7 +30,7 @@ def trip_purpose_calc_row_size(choosers, spec, trace_label):
     sizer = chunk.RowSizeEstimator(trace_label)
 
     chooser_row_size = len(choosers.columns)
-    spec_columns = spec.shape[1] - len(PROBS_JOIN_COLUMNS)
+    spec_columns = spec.shape[1] - len(probs_join_cols)
 
     sizer.add_elements(chooser_row_size + spec_columns, 'choosers')
 
@@ -41,7 +38,7 @@ def trip_purpose_calc_row_size(choosers, spec, trace_label):
     return row_size
 
 
-def choose_intermediate_trip_purpose(trips, probs_spec, trace_hh_id, trace_label):
+def choose_intermediate_trip_purpose(trips, probs_spec, probs_join_cols, use_depart_time, trace_hh_id, trace_label):
     """
     chose purpose for intermediate trips based on probs_spec
     which assigns relative weights (summing to 1) to the possible purpose choices
@@ -51,7 +48,9 @@ def choose_intermediate_trip_purpose(trips, probs_spec, trace_hh_id, trace_label
     purpose: pandas.Series of purpose (str) indexed by trip_id
     """
 
-    non_purpose_cols = PROBS_JOIN_COLUMNS + ['depart_range_start', 'depart_range_end']
+    non_purpose_cols = probs_join_cols
+    if use_depart_time:
+         non_purpose_cols += ['depart_range_start', 'depart_range_end']
     purpose_cols = [c for c in probs_spec.columns if c not in non_purpose_cols]
 
     num_trips = len(trips.index)
@@ -62,36 +61,39 @@ def choose_intermediate_trip_purpose(trips, probs_spec, trace_hh_id, trace_label
     probs_spec.loc[:, purpose_cols] = probs_spec.loc[:, purpose_cols].div(sum_probs, axis=0)
 
     # left join trips to probs (there may be multiple rows per trip for multiple depart ranges)
-    choosers = pd.merge(trips.reset_index(), probs_spec, on=PROBS_JOIN_COLUMNS,
+    choosers = pd.merge(trips.reset_index(), probs_spec, on=probs_join_cols,
                         how='left').set_index('trip_id')
     chunk.log_df(trace_label, 'choosers', choosers)
 
-    # select the matching depart range (this should result on in exactly one chooser row per trip)
-    chooser_probs = \
-        (choosers.start >= choosers['depart_range_start']) & (choosers.start <= choosers['depart_range_end'])
+    if use_depart_time:
 
-    # if we failed to match a row in probs_spec
-    if chooser_probs.sum() < num_trips:
-        # this can happen if the spec doesn't have probs for the trips matching a trip's probs_join_cols
-        missing_trip_ids = trips.index[~trips.index.isin(choosers.index[chooser_probs])].values
-        unmatched_choosers = choosers[choosers.index.isin(missing_trip_ids)]
-        unmatched_choosers = unmatched_choosers[['person_id', 'start'] + non_purpose_cols]
+        # select the matching depart range (this should result on in exactly one chooser row per trip)
+        chooser_probs = \
+            (choosers.start >= choosers['depart_range_start']) & (choosers.start <= choosers['depart_range_end'])
 
-        # join to persons for better diagnostics
-        persons = inject.get_table('persons').to_frame()
-        persons_cols = ['age', 'is_worker', 'is_student', 'is_gradeschool', 'is_highschool', 'is_university']
-        unmatched_choosers = pd.merge(unmatched_choosers, persons[persons_cols],
-                                      left_on='person_id', right_index=True, how='left')
+        # if we failed to match a row in probs_spec
+        if chooser_probs.sum() < num_trips:
 
-        file_name = '%s.UNMATCHED_PROBS' % trace_label
-        logger.error("%s %s of %s intermediate trips could not be matched to probs based on join columns  %s" %
-                     (trace_label, len(unmatched_choosers), len(choosers), probs_join_cols))
-        logger.info("Writing %s unmatched choosers to %s" % (len(unmatched_choosers), file_name,))
-        tracing.write_csv(unmatched_choosers, file_name=file_name, transpose=False)
-        raise RuntimeError("Some trips could not be matched to probs based on join columns %s." % probs_join_cols)
+            # this can happen if the spec doesn't have probs for the trips matching a trip's probs_join_cols
+            missing_trip_ids = trips.index[~trips.index.isin(choosers.index[chooser_probs])].values
+            unmatched_choosers = choosers[choosers.index.isin(missing_trip_ids)]
+            unmatched_choosers = unmatched_choosers[['person_id', 'start'] + non_purpose_cols]
 
-    # select the matching depart range (this should result on in exactly one chooser row per trip)
-    choosers = choosers[chooser_probs]
+            # join to persons for better diagnostics
+            persons = inject.get_table('persons').to_frame()
+            persons_cols = ['age', 'is_worker', 'is_student', 'is_gradeschool', 'is_highschool', 'is_university']
+            unmatched_choosers = pd.merge(unmatched_choosers, persons[[col for col in persons_cols if col in persons.columns]],
+                                          left_on='person_id', right_index=True, how='left')
+
+            file_name = '%s.UNMATCHED_PROBS' % trace_label
+            logger.error("%s %s of %s intermediate trips could not be matched to probs based on join columns  %s" %
+                         (trace_label, len(unmatched_choosers), len(choosers), probs_join_cols))
+            logger.info("Writing %s unmatched choosers to %s" % (len(unmatched_choosers), file_name,))
+            tracing.write_csv(unmatched_choosers, file_name=file_name, transpose=False)
+            raise RuntimeError("Some trips could not be matched to probs based on join columns %s." % probs_join_cols)
+
+        # select the matching depart range (this should result on in exactly one chooser row per trip)
+        choosers = choosers[chooser_probs]
 
     # choosers should now match trips row for row
     assert choosers.index.identical(trips.index)
@@ -119,8 +121,10 @@ def run_trip_purpose(
     For each intermediate stop on a tour (i.e. trip other than the last trip outbound or inbound)
     Each trip is assigned a purpose based on an observed frequency distribution
 
-    The distribution is segmented by tour purpose, tour direction, person type,
-    and, optionally, trip depart time .
+    The distribution should always be segmented by tour purpose and tour direction. By default it is also
+    segmented by person type. The join columns can be overwritten using the "probs_join_cols" parameter in
+    the model settings. The model will attempt to segment by trip depart time as well if necessary
+    and depart time ranges are specified in the probability lookup table.
 
     Returns
     -------
@@ -128,6 +132,8 @@ def run_trip_purpose(
     """
 
     model_settings = config.read_model_settings('trip_purpose.yaml')
+    default_join_cols = ['primary_purpose', 'outbound', 'person_type']
+    probs_join_cols = model_settings.get('probs_join_cols', default_join_cols)
     probs_spec = trip_purpose_probs()
 
     result_list = []
@@ -140,7 +146,7 @@ def run_trip_purpose(
 
     # - last trip of inbound tour gets home (or work for atwork subtours)
     purpose = trips_df.primary_purpose[last_trip & ~trips_df.outbound]
-    purpose = pd.Series(np.where(purpose == 'atwork', 'Work', 'Home'), index=purpose.index)
+    purpose = pd.Series(np.where(purpose == 'atwork', 'work', 'home'), index=purpose.index)
     result_list.append(purpose)
     logger.info("assign purpose to %s last inbound trips", purpose.shape[0])
 
@@ -157,15 +163,19 @@ def run_trip_purpose(
             locals_dict=locals_dict,
             trace_label=trace_label)
 
-    row_size = chunk_size and trip_purpose_calc_row_size(trips_df, probs_spec, trace_label)
+    use_depart_time = model_settings.get('use_depart_time', True)
+    row_size = chunk_size and trip_purpose_calc_row_size(
+        trips_df, probs_spec, probs_join_cols, trace_label=trace_label)
 
     for i, trips_chunk, chunk_trace_label in \
             chunk.adaptive_chunked_choosers(trips_df, chunk_size, row_size, trace_label):
-
+        
         choices = choose_intermediate_trip_purpose(
             trips_chunk,
             probs_spec,
-            trace_hh_id,
+            probs_join_cols=probs_join_cols,
+            use_depart_time=use_depart_time,
+            trace_hh_id=trace_hh_id,
             trace_label=chunk_trace_label)
 
         result_list.append(choices)

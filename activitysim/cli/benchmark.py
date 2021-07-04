fad157225afe2e985281a94c100a7679623b376a
Jeff Newman <jeffnewman@camsys.com>
2021-07-04 15:12:18 -0500

    locally manage json

--- activitysim/cli/benchmark.py
+++ activitysim/cli/benchmark.py
@@ -1,6 +1,75 @@
 import os
 import sys
+import json
 
+ASV_CONFIG = {
+    # The version of the config file format.  Do not change, unless
+    # you know what you are doing.
+    "version": 1,
+
+    # The name of the project being benchmarked
+    "project": "activitysim",
+
+    #The project's homepage
+    "project_url": "https://activitysim.github.io/",
+
+    # The URL or local path of the source code repository for the
+    # project being benchmarked
+    "repo": ".",
+
+    # The tool to use to create environments.
+    "environment_type": "conda",
+
+    # the base URL to show a commit for the project.
+    "show_commit_url": "http://github.com/ActivitySim/activitysim/commit/",
+
+    # The Pythons you'd like to test against.  If not provided, defaults
+    # to the current version of Python used to run `asv`.
+    # "pythons": ["2.7", "3.6"],
+
+    # The list of conda channel names to be searched for benchmark
+    # dependency packages in the specified order
+    "conda_channels": ["conda-forge"],
+
+    # The matrix of dependencies to test.  Each key is the name of a
+    # package (in PyPI) and the values are version numbers.  An empty
+    # list or empty string indicates to just test against the default
+    # (latest) version. null indicates that the package is to not be
+    # installed. If the package to be tested is only available from
+    # PyPi, and the 'environment_type' is conda, then you can preface
+    # the package name by 'pip+', and the package will be installed via
+    # pip (with all the conda available packages installed first,
+    # followed by the pip installed packages).
+    "matrix": {
+        "pyarrow": [],
+        "numpy": [],
+        "openmatrix": [],
+        "pandas": ["1.2"],
+        "pyyaml": [],
+        "pytables": [],
+        "toolz": [],
+        "orca": [],
+        "psutil": [],
+        "requests": [],
+        "numba": [],
+        "coverage": [],
+        "pytest": [],
+        "ruamel.yaml": [],
+        "cytoolz": []
+    },
+
+    # The directory (relative to the current directory) to cache the Python
+    # environments in.  If not provided, defaults to "env"
+    "env_dir": "../activitysim-asv/env",
+
+    # The directory (relative to the current directory) that raw benchmark
+    # results are stored in.  If not provided, defaults to "results".
+    "results_dir": "../activitysim-asv/results",
+
+    # The directory (relative to the current directory) that the html tree
+    # should be written to.  If not provided, defaults to "html".
+    "html_dir": "../activitysim-asv/html",
+}
 
 def make_asv_argparser(parser):
     """
@@ -33,12 +102,22 @@ def make_asv_argparser(parser):
             continue
         subparser = commands[str(command)].setup_arguments(subparsers)
         common_args.add_global_arguments(subparser)
+        subparser.add_argument(
+            "--workspace", "-w",
+            help="benchmarking workspace directory",
+            default=".",
+        )
         del commands[command]
 
     for name, command in sorted(commands.items()):
         if str(command) in hide_commands:
             continue
         subparser = command.setup_arguments(subparsers)
+        subparser.add_argument(
+            "--workspace", "-w",
+            help="benchmarking workspace directory",
+            default=".",
+        )
         common_args.add_global_arguments(subparser)
 
     parser.set_defaults(afunc=benchmark)
@@ -50,18 +129,45 @@ def benchmark(args):
     from asv.console import log
     from asv import util
 
-    print("benchmarking activitysim...")
-
     log.enable(args.verbose)
 
-    args.config = os.path.abspath(args.config)
+    log.info("<== benchmarking activitysim ==>")
 
-    # Use the path to the config file as the cwd for the remainder of
-    # the run
-    dirname = os.path.dirname(args.config)
-    os.chdir(dirname)
+    # workspace
+    args.workspace = os.path.abspath(args.workspace)
+    if not os.path.isdir(args.workspace):
+        raise NotADirectoryError(args.workspace)
+    log.info(f" workspace: {args.workspace}")
+    os.chdir(args.workspace)
 
-    print("working directory:", dirname)
+    from .. import __path__ as pkg_path
+    log.info(f" activitysim installation: {pkg_path[0]}")
+
+    repo_dir = os.path.normpath(
+        os.path.join(pkg_path[0], "..")
+    )
+    git_dir = os.path.normpath(
+        os.path.join(repo_dir, ".git")
+    )
+    local_git = os.path.exists(git_dir)
+    log.info(f" local git repo available: {local_git}")
+
+    asv_config = ASV_CONFIG.copy()
+    if local_git:
+        asv_config["repo"] = os.path.relpath(args.workspace, repo_dir)
+    else:
+        asv_config["repo"] = "https://github.com/ActivitySim/activitysim.git"
+
+    conf_file = os.path.normpath(
+        os.path.join(args.workspace, "asv.conf.json")
+    )
+    with open(conf_file, 'wt') as jf:
+        json.dump(asv_config, jf)
+
+    if args.config and args.config != "asv.conf.json":
+        raise ValueError("activitysim manages the asv config json file itself, do not use --config")
+    args.config = os.path.abspath(conf_file)
+
 
     try:
         result = args.func(args)

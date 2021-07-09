040d8a6723a2294a2eebde2d405caac58ce539d2
Max <gardner.max@gmail.com>
2021-07-09 10:34:49 -0700

    add recipe selection to tvpb logsum wrapper, trip mode choice

--- activitysim/abm/models/trip_mode_choice.py
+++ activitysim/abm/models/trip_mode_choice.py
@@ -110,12 +110,22 @@ def trip_mode_choice(
 
         tvpb_logsum_odt = tvpb.wrap_logsum(orig_key=orig_col, dest_key=dest_col,
                                            tod_key='trip_period', segment_key='demographic_segment',
-                                           cache_choices=True,
+                                           recipe='trip_mode_choice', cache_choices=True,
                                            trace_label=trace_label, tag='tvpb_logsum_odt')
         skims.update({
             'tvpb_logsum_odt': tvpb_logsum_odt,
         })
 
+        # This if-clause gives the user the option of NOT inheriting constants
+        # from the tvpb settings. previously, these constants were inherited 
+        # automatically, which had the undesirable effect of overwriting any
+        # trip mode choice model constants/coefficients that shared the same
+        # name. The default behavior is still the same (True), but the user
+        # can now avoid any chance of squashing these local variables by 
+        # adding `use_TVPB_constants: False` to the trip_mode_choice.yaml file.
+        # the tvpb will still use the constants as defined in the recipe
+        # specified above in `tvpb.wrap_logsum()` but they will not be used
+        # in the trip mode choice expressions.
         if model_settings.get('use_TVPB_constants', True):
             constants.update(network_los.setting('TVPB_SETTINGS.tour_mode_choice.CONSTANTS'))
 

ae7cf473f85370c4653dc7e5d9189373e200a1a4
albabnoor <albab.un.noor@gmail.com>
2021-07-09 10:46:03 -0700

    update example semcog

--- activitysim/examples/example_semcog/extensions/__init__.py
+++ activitysim/examples/example_semcog/extensions/__init__.py
@@ -2,3 +2,6 @@ from . import work_from_home
 from . import telecommute_frequency
 from . import transit_pass_subsidy
 from . import transit_pass_ownership
+from . import parking_location_choice_at_university
+from . import stop_frequency_university_parking
+from . import university_location_zone_override

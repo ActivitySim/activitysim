db11669ed9d43348c5daf517b24fa44e51967140
Jeff Newman <jeffnewman@camsys.com>
2021-09-13 10:47:18 -0500

    elapsed time logging

--- activitysim/core/tracing.py
+++ activitysim/core/tracing.py
@@ -29,6 +29,19 @@ LOGGING_CONF_FILE_NAME = 'logging.yaml'
 logger = logging.getLogger(__name__)
 
 
+
+class ElapsedTimeFormatter(logging.Formatter):
+    def format(self, record):
+        duration_milliseconds = record.relativeCreated
+        hours, rem = divmod(duration_milliseconds / 1000, 3600)
+        minutes, seconds = divmod(rem, 60)
+        if hours:
+            record.elapsedTime = ("{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+        else:
+            record.elapsedTime = ("{:0>2}:{:05.2f}".format(int(minutes), seconds))
+        return super(ElapsedTimeFormatter, self).format(record)
+
+
 def extend_trace_label(trace_label, extension):
     if trace_label:
         trace_label = "%s.%s" % (trace_label, extension)

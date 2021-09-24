9f1e0e4a5054770d15aff99d928a29e971b7549d
Jeff Newman <jeffnewman@camsys.com>
2021-09-24 17:18:12 -0500

    cwise template blackened

--- activitysim/benchmarking/benchmarks/componentwise_template.py
+++ activitysim/benchmarking/benchmarks/componentwise_template.py
@@ -1,35 +1,38 @@
 import argparse
-import sys
-import os
 import logging
-import time
-import yaml
 import multiprocessing
+import os
+import sys
+import time
 from datetime import timedelta
 from functools import partial
+
+import yaml
 from activitysim.benchmarking import componentwise, modify_yaml, workspace
 from activitysim.cli.create import get_example
 
 logger = logging.getLogger("activitysim.benchmarking")
 benchmarking_directory = workspace.get_dir()
 
+
 def f_setup_cache(
-        EXAMPLE_NAME,
-        COMPONENT_NAMES,
-        BENCHMARK_SETTINGS,
-        CONFIGS_DIRS=("configs",),
-        DATA_DIR='data',
-        OUTPUT_DIR='output',
-        SETTINGS_FILENAME="settings.yaml",
-        SKIP_COMPONENT_NAMES=None,
-        NUM_PROCESSES=None,
-        SKIM_CACHE=True,
+    EXAMPLE_NAME,
+    COMPONENT_NAMES,
+    BENCHMARK_SETTINGS,
+    CONFIGS_DIRS=("configs",),
+    DATA_DIR="data",
+    OUTPUT_DIR="output",
+    SETTINGS_FILENAME="settings.yaml",
+    SKIP_COMPONENT_NAMES=None,
+    NUM_PROCESSES=None,
+    SKIM_CACHE=True,
 ):
     models = None
     try:
         if workspace.get_dir() is None:
             from asv.console import log
-            for k,v in os.environ.items():
+
+            for k, v in os.environ.items():
                 log.error(f" env {k}: {v}")
             raise RuntimeError("workspace unavailable")
         os.makedirs(os.path.join(local_dir(), "models"), exist_ok=True)
@@ -40,17 +43,23 @@ def f_setup_cache(
         )
         settings_filename = os.path.join(model_dir(EXAMPLE_NAME), SETTINGS_FILENAME)
         for config_settings_dir in CONFIGS_DIRS:
-            settings_filename = os.path.join(model_dir(EXAMPLE_NAME), config_settings_dir, SETTINGS_FILENAME)
+            settings_filename = os.path.join(
+                model_dir(EXAMPLE_NAME), config_settings_dir, SETTINGS_FILENAME
+            )
             if os.path.exists(settings_filename):
-                with open(settings_filename, 'rt') as f:
-                    models = yaml.load(f, Loader=yaml.loader.SafeLoader).get('models')
+                with open(settings_filename, "rt") as f:
+                    models = yaml.load(f, Loader=yaml.loader.SafeLoader).get("models")
                 break
         if models is None and SETTINGS_FILENAME != "settings.yaml":
             for config_settings_dir in CONFIGS_DIRS:
-                settings_filename = os.path.join(model_dir(EXAMPLE_NAME), config_settings_dir, "settings.yaml")
+                settings_filename = os.path.join(
+                    model_dir(EXAMPLE_NAME), config_settings_dir, "settings.yaml"
+                )
                 if os.path.exists(settings_filename):
-                    with open(settings_filename, 'rt') as f:
-                        models = yaml.load(f, Loader=yaml.loader.SafeLoader).get('models')
+                    with open(settings_filename, "rt") as f:
+                        models = yaml.load(f, Loader=yaml.loader.SafeLoader).get(
+                            "models"
+                        )
                     break
         if models is None:
             raise ValueError(f"missing list of models from configs/{SETTINGS_FILENAME}")
@@ -58,8 +67,7 @@ def f_setup_cache(
         for cname in COMPONENT_NAMES:
             try:
                 last_component_to_benchmark = max(
-                    models.index(cname),
-                    last_component_to_benchmark
+                    models.index(cname), last_component_to_benchmark
                 )
             except ValueError:
                 if cname not in models:
@@ -75,17 +83,19 @@ def f_setup_cache(
             models=pre_run_model_list,
             checkpoints=True,
             trace_hh_id=None,
-            chunk_training_mode='off',
+            chunk_training_mode="off",
         )
         if NUM_PROCESSES is not None:
-            settings_changes['num_processes'] = NUM_PROCESSES
+            settings_changes["num_processes"] = NUM_PROCESSES
         modify_yaml(
             settings_filename,
             **BENCHMARK_SETTINGS,
             **settings_changes,
         )
         for config_network_los_dir in CONFIGS_DIRS:
-            network_los_filename = os.path.join(model_dir(EXAMPLE_NAME), config_network_los_dir, "network_los.yaml")
+            network_los_filename = os.path.join(
+                model_dir(EXAMPLE_NAME), config_network_los_dir, "network_los.yaml"
+            )
             if os.path.exists(network_los_filename):
                 modify_yaml(
                     network_los_filename,
@@ -102,26 +112,36 @@ def f_setup_cache(
         # delete the token (or the whole model file) when a structural change
         # in the model happens such that re-checkpointing is needed (this should
         # happen rarely).
-        token_file = os.path.join(model_dir(EXAMPLE_NAME), OUTPUT_DIR, 'benchmark-setup-token.txt')
+        token_file = os.path.join(
+            model_dir(EXAMPLE_NAME), OUTPUT_DIR, "benchmark-setup-token.txt"
+        )
         if not os.path.exists(token_file):
             try:
-                componentwise.pre_run(model_dir(EXAMPLE_NAME), CONFIGS_DIRS, DATA_DIR, OUTPUT_DIR, SETTINGS_FILENAME)
+                componentwise.pre_run(
+                    model_dir(EXAMPLE_NAME),
+                    CONFIGS_DIRS,
+                    DATA_DIR,
+                    OUTPUT_DIR,
+                    SETTINGS_FILENAME,
+                )
             except Exception as err:
-                with open(token_file, 'wt') as f:
+                with open(token_file, "wt") as f:
                     f.write(f"error {err}")
                 raise
             else:
-                with open(token_file, 'wt') as f:
+                with open(token_file, "wt") as f:
                     # We write the commit into the token, in case that is useful
                     # to developers to decide if the checkpointed pipeline is
                     # out of date.
-                    asv_commit = os.environ.get('ASV_COMMIT', 'ASV_COMMIT_UNKNOWN')
+                    asv_commit = os.environ.get("ASV_COMMIT", "ASV_COMMIT_UNKNOWN")
                     f.write(asv_commit)
 
     except Exception as err:
         import traceback
+
         traceback.print_exc()
         raise
+
 
 def local_dir():
     if benchmarking_directory is not None:
@@ -134,32 +154,39 @@ def model_dir(example_name):
 
 
 def generate_component_timings(
-        componentname,
-        EXAMPLE_NAME,
-        CONFIGS_DIRS,
-        DATA_DIR,
-        OUTPUT_DIR,
-        PRELOAD_INJECTABLES,
-        REPEAT,
-        NUMBER,
-        TIMEOUT,
+    componentname,
+    EXAMPLE_NAME,
+    CONFIGS_DIRS,
+    DATA_DIR,
+    OUTPUT_DIR,
+    PRELOAD_INJECTABLES,
+    REPEAT,
+    NUMBER,
+    TIMEOUT,
 ):
-
     class ComponentTiming:
         component_name = componentname
         repeat = REPEAT
         number = NUMBER
         timeout = TIMEOUT
+
         def setup(self):
             componentwise.setup_component(
-                self.component_name, model_dir(EXAMPLE_NAME), PRELOAD_INJECTABLES,
-                CONFIGS_DIRS, DATA_DIR, OUTPUT_DIR,
+                self.component_name,
+                model_dir(EXAMPLE_NAME),
+                PRELOAD_INJECTABLES,
+                CONFIGS_DIRS,
+                DATA_DIR,
+                OUTPUT_DIR,
             )
+
         def teardown(self):
             componentwise.teardown_component(self.component_name)
+
         def time_component(self):
             componentwise.run_component(self.component_name)
-        #time_component.benchmark_name = f"{__name__}.time_component.{componentname}"
+
+        # time_component.benchmark_name = f"{__name__}.time_component.{componentname}"
         time_component.pretty_name = f"{EXAMPLE_NAME}:{componentname}"
 
     ComponentTiming.__name__ = f"{componentname}"
@@ -168,24 +195,24 @@ def generate_component_timings(
 
 
 def generate_complete(
-        EXAMPLE_NAME,
-        CONFIGS_DIRS,
-        DATA_DIR,
-        OUTPUT_DIR,
-        TIMEOUT,
-        COMPONENT_NAMES,
-        BENCHMARK_SETTINGS,
-        SETTINGS_FILENAME="settings_mp.yaml",
-        SKIM_CACHE=True,
-        MAX_PROCESSES=10,
+    EXAMPLE_NAME,
+    CONFIGS_DIRS,
+    DATA_DIR,
+    OUTPUT_DIR,
+    TIMEOUT,
+    COMPONENT_NAMES,
+    BENCHMARK_SETTINGS,
+    SETTINGS_FILENAME="settings_mp.yaml",
+    SKIM_CACHE=True,
+    MAX_PROCESSES=10,
+    HOUSEHOLD_SAMPLE=None,
 ):
-
     class mp_complete:
         repeat = 1
         number = 1
-        timeout = TIMEOUT*100
+        timeout = TIMEOUT * 100
         component_names = COMPONENT_NAMES
-        benchmark_settings = BENCHMARK_SETTINGS
+        benchmark_settings = BENCHMARK_SETTINGS.copy()
         skim_cache = SKIM_CACHE
 
         def setup(self):
@@ -193,23 +220,33 @@ def generate_complete(
             # that we do not overwrite the checkpointed pipeline used by
             # single-component benchmarks.
             f_setup_cache(
-                EXAMPLE_NAME, self.component_names, self.benchmark_settings,
-                CONFIGS_DIRS, DATA_DIR, OUTPUT_DIR + "MP",
+                EXAMPLE_NAME,
+                self.component_names,
+                self.benchmark_settings,
+                CONFIGS_DIRS,
+                DATA_DIR,
+                OUTPUT_DIR + "MP",
                 SETTINGS_FILENAME=SETTINGS_FILENAME,
                 NUM_PROCESSES=max(
-                    min(multiprocessing.cpu_count()-2, MAX_PROCESSES),
+                    min(multiprocessing.cpu_count() - 2, MAX_PROCESSES),
                     2,
                 ),
                 SKIM_CACHE=self.skim_cache,
             )
 
         def time_complete(self):
-            INJECTABLES = ['data_dir', 'configs_dir', 'output_dir', 'settings_file_name']
-            from activitysim.core import mp_tasks, inject, pipeline, config
+            INJECTABLES = [
+                "data_dir",
+                "configs_dir",
+                "output_dir",
+                "settings_file_name",
+            ]
+            from activitysim.core import config, inject, mp_tasks, pipeline
+
             injectables = {k: inject.get_injectable(k) for k in INJECTABLES}
             mp_tasks.run_multiprocess(injectables)
             assert not pipeline.is_open()
-            if config.setting('cleanup_pipeline_after_run', False):
+            if config.setting("cleanup_pipeline_after_run", False):
                 pipeline.cleanup_pipeline()
 
         time_complete.pretty_name = f"{EXAMPLE_NAME}:MP-Complete"
@@ -218,20 +255,20 @@ def generate_complete(
 
 
 def apply_template(
-        GLOBALS,
-        EXAMPLE_NAME,
-        CONFIGS_DIRS,
-        DATA_DIR,
-        OUTPUT_DIR,
-        PRELOAD_INJECTABLES,
-        REPEAT,
-        NUMBER,
-        TIMEOUT,
-        COMPONENT_NAMES,
-        BENCHMARK_SETTINGS,
-        SKIM_CACHE=True,
-        MP_SAMPLE_SIZE=0,
-        MAX_PROCESSES=10,
+    GLOBALS,
+    EXAMPLE_NAME,
+    CONFIGS_DIRS,
+    DATA_DIR,
+    OUTPUT_DIR,
+    PRELOAD_INJECTABLES,
+    REPEAT,
+    NUMBER,
+    TIMEOUT,
+    COMPONENT_NAMES,
+    BENCHMARK_SETTINGS,
+    SKIM_CACHE=True,
+    MP_SAMPLE_SIZE=0,
+    MAX_PROCESSES=10,
 ):
     # def setup_cache():
     #     f_setup_cache(
@@ -239,8 +276,8 @@ def apply_template(
     #         CONFIGS_DIRS, DATA_DIR, OUTPUT_DIR,
     #         SKIM_CACHE=SKIM_CACHE,
     #     )
-
-    GLOBALS["setup_cache"] = setup_cache
+    #
+    # GLOBALS["setup_cache"] = setup_cache
 
     for cname in COMPONENT_NAMES:
         GLOBALS[cname] = generate_component_timings(
@@ -256,7 +293,7 @@ def apply_template(
         )
 
     BENCHMARK_SETTINGS_COMPLETE = BENCHMARK_SETTINGS.copy()
-    BENCHMARK_SETTINGS_COMPLETE['households_sample_size'] = MP_SAMPLE_SIZE
+    BENCHMARK_SETTINGS_COMPLETE["households_sample_size"] = MP_SAMPLE_SIZE
 
     GLOBALS[f"mp_complete"] = generate_complete(
         EXAMPLE_NAME,

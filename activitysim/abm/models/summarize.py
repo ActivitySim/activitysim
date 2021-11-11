97ca9acc026fb3c6a0fbf6d48d5b427e8d74ab4a
Clint Daniels <clint.daniels@wsp.com>
2021-11-10 16:56:03 -0800

    First summarize commit.

--- activitysim/abm/models/summarize.py
+++ activitysim/abm/models/summarize.py
@@ -14,30 +14,12 @@ logger = logging.getLogger(__name__)
 
 
 @inject.step()
-def write_summaries(output_dir):
-
-    summary_settings_name = 'output_summaries'
-    summary_file_name = 'summaries.txt'
-
-    summary_settings = setting(summary_settings_name)
-
-    if summary_settings is None:
-        logger.info("No {summary_settings_name} specified in settings file. Nothing to write.")
-        return
-
-    summary_dict = summary_settings
-
-    mode = 'wb' if sys.version_info < (3,) else 'w'
-    with open(config.output_file_path(summary_file_name), mode) as output_file:
-
-        for table_name, column_names in summary_dict.items():
-
-            df = pipeline.get_table(table_name)
-
-            for c in column_names:
-                n = 100
-                empty = (df[c] == '') | df[c].isnull()
-
-                print(f"\n### {table_name}.{c} type: {df.dtypes[c]} rows: {len(df)} ({empty.sum()} empty)\n\n",
-                      file=output_file)
-                print(df[c].value_counts().nlargest(n), file=output_file)
+def summarize(households_merged, tours_merged):
+    """
+        summarize is a standard model which uses expression files
+        to reduce tables
+        """
+    trace_label = 'summarize'
+    model_settings_file_name = 'summarize.yaml'
+    model_settings = config.read_model_settings(model_settings_file_name)
+    pass

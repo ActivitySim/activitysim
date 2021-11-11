8323213486872d922650d6989a45269c6c9c649a
Clint Daniels <clint.daniels@wsp.com>
2021-11-11 15:06:07 -0800

    Quick concept test for summarize

--- activitysim/abm/models/summarize.py
+++ activitysim/abm/models/summarize.py
@@ -1,5 +1,6 @@
 # ActivitySim
 # See full license in LICENSE.txt.
+import os
 import logging
 import sys
 import pandas as pd
@@ -14,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 
 @inject.step()
-def summarize(households_merged, tours_merged):
+def summarize(tours_merged):
     """
         summarize is a standard model which uses expression files
         to reduce tables
@@ -22,4 +23,24 @@ def summarize(households_merged, tours_merged):
     trace_label = 'summarize'
     model_settings_file_name = 'summarize.yaml'
     model_settings = config.read_model_settings(model_settings_file_name)
-    pass
+
+    locals_d = {
+        'df': tours_merged
+    }
+
+
+    for segment in model_settings['SPEC_SEGMENTS']:
+        table = segment['table']
+        spec_name = segment['spec']
+        output_location = segment['output'] if 'output' in segment else 'summaries'
+        os.makedirs(config.output_file_path(output_location), exist_ok=True)
+
+        spec = pd.read_csv(config.config_file_path(spec_name))
+
+        for i, row in spec.iterrows():
+            out_file = row['Output']
+            expr = row['Expression']
+
+            resultset = eval(expr, globals(), locals_d)
+            resultset.to_csv(config.output_file_path(os.path.join(output_location, f'{out_file}.csv')))
+

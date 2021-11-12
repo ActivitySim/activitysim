44be4510af632cfb6ab85c51b2202080e6e0f602
Chester Harvey <chesterharvey@gmail.com>
2021-11-11 17:54:42 -0700

    generalized output path

--- activitysim/abm/test/test_misc/test_summarize.py
+++ activitysim/abm/test/test_misc/test_summarize.py
@@ -7,13 +7,12 @@ from .setup_utils import setup_dirs
 
 @pytest.fixture(scope='module')
 def data_path():
-    return os.path.join('activitysim', 'examples', 'example_mtc', 'output')
+    return os.path.join('..', '..', '..', 'examples', 'example_mtc', 'output')
 
 @pytest.fixture(scope='module')
 def tours(data_path):
      tour_path = os.path.join(data_path, 'final_tours.csv')
      return pd.read_csv(tour_path)
-
 
 def test_summarize(tours):
     setup_dirs()

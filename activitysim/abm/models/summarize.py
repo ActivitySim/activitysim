981803ddabc799e8f4f2775aabd6afe44f2bc890
Chester Harvey <chesterharvey@gmail.com>
2021-11-16 16:35:05 -0700

    Added functionality for summarize to access pipeline if available or be tested with csv inputs

--- activitysim/abm/models/summarize.py
+++ activitysim/abm/models/summarize.py
@@ -13,21 +13,15 @@ from activitysim.core.config import setting
 
 logger = logging.getLogger(__name__)
 
-
 @inject.step()
-def summarize(tours_merged):
+def summarize(locals_d=None):
     """
-        summarize is a standard model which uses expression files
-        to reduce tables
-        """
+    summarize is a standard model which uses expression files
+    to reduce tables
+    """
     trace_label = 'summarize'
     model_settings_file_name = 'summarize.yaml'
     model_settings = config.read_model_settings(model_settings_file_name)
-
-    locals_d = {
-        'df': tours_merged
-    }
-
 
     for segment in model_settings['SPEC_SEGMENTS']:
         table = segment['table']
@@ -35,9 +29,16 @@ def summarize(tours_merged):
         output_location = segment['output'] if 'output' in segment else 'summaries'
         os.makedirs(config.output_file_path(output_location), exist_ok=True)
 
+        # Go get specified tables from the pipeline unless they are supplied as a parameter
+        if not locals_d:
+            locals_d = {
+                table: pipeline.get_table(table)
+            }
+
         spec = pd.read_csv(config.config_file_path(spec_name))
 
         for i, row in spec.iterrows():
+
             out_file = row['Output']
             expr = row['Expression']
 
--- activitysim/abm/test/test_misc/test_summarize.py
+++ activitysim/abm/test/test_misc/test_summarize.py
@@ -2,7 +2,8 @@ import os
 import pandas as pd
 import pytest
 
-from activitysim.abm.models import summarize
+from activitysim.core import pipeline
+from activitysim.abm.models.summarize import summarize
 from .setup_utils import setup_dirs
 
 @pytest.fixture(scope='module')
@@ -10,10 +11,60 @@ def data_path():
     return os.path.join('..', '..', '..', 'examples', 'example_mtc', 'output')
 
 @pytest.fixture(scope='module')
-def tours(data_path):
-     tour_path = os.path.join(data_path, 'final_tours.csv')
-     return pd.read_csv(tour_path)
+def trips(data_path):
+     return pd.read_csv(os.path.join(data_path, 'final_trips.csv'))
 
-def test_summarize(tours):
+@pytest.fixture(scope='module')
+def tours(data_path):
+     return pd.read_csv(os.path.join(data_path, 'final_tours.csv'))
+
+@pytest.fixture(scope='module')
+def tables(trips, tours):
+     return {
+        'trips': trips,
+        'tours': tours,
+     }
+
+# def test_summarize(tours):
+#     setup_dirs()
+#     summarize.summarize(tours)
+
+
+# @pytest.fixture(scope='module')
+# def synthesize_pipeline(tables):
+#     if not pipeline.is_open():
+#         try:
+#             # Open the pipeline from an available h5 file
+#             # (this makes sure an available h5 file isn't overwritten by a new pipeline)
+#             ipeline.open_pipeline('_')
+#         except:
+#             # Otherwise, create a new pipeline
+#             pipeline.open_pipeline()
+#     test_checkpoint = 'test_summarize'
+#     # Make a new test checkpoint if it doesn't exist already
+#     if test_checkpoint not in pipeline.get_checkpoints().checkpoint_name:
+#         pipeline.add_checkpoint('test_summarize')
+#     # Otherwise, go to that checkpoint
+#     else:
+#         pipeline.load_checkpoint('test_summarize')
+#     # Replace specified tables with inputs 
+#     for name, df in tables.items():
+#         pipeline.replace_table(name, df)
+    
+
+# def test_summarize(tables=None):
+#     setup_dirs()
+#     if tables:
+#         synthesize_pipeline(tables)
+#     else:
+#         if not pipeline.is_open():
+#             pipeline.open_pipeline('_')
+#     summarize()
+
+
+def test_summarize(tables):
     setup_dirs()
-    summarize.summarize(tours)
+    summarize(tables)
+
+
+

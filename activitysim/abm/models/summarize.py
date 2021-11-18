ca3b20562a92cfca7f5b963aa0ff9bcfac37f092
Clint Daniels <clint.daniels@wsp.com>
2021-11-17 17:44:43 -0800

    Initialize test via pipeline

--- activitysim/abm/models/summarize.py
+++ activitysim/abm/models/summarize.py
@@ -2,19 +2,16 @@
 # See full license in LICENSE.txt.
 import os
 import logging
-import sys
 import pandas as pd
 
 from activitysim.core import pipeline
 from activitysim.core import inject
 from activitysim.core import config
 
-from activitysim.core.config import setting
-
 logger = logging.getLogger(__name__)
 
 @inject.step()
-def summarize(locals_d=None):
+def summarize():
     """
     summarize is a standard model which uses expression files
     to reduce tables
@@ -30,10 +27,9 @@ def summarize(locals_d=None):
         os.makedirs(config.output_file_path(output_location), exist_ok=True)
 
         # Go get specified tables from the pipeline unless they are supplied as a parameter
-        if not locals_d:
-            locals_d = {
-                table: pipeline.get_table(table)
-            }
+        locals_d = {
+            table: pipeline.get_table(table)
+        }
 
         spec = pd.read_csv(config.config_file_path(spec_name))
 
--- activitysim/abm/test/test_misc/test_summarize.py
+++ activitysim/abm/test/test_misc/test_summarize.py
@@ -2,69 +2,15 @@ import os
 import pandas as pd
 import pytest
 
+### import models is necessary to initalize the model steps with orca
+from activitysim.abm import models
+
 from activitysim.core import pipeline
-from activitysim.abm.models.summarize import summarize
-from .setup_utils import setup_dirs
-
-@pytest.fixture(scope='module')
-def data_path():
-    return os.path.join('..', '..', '..', 'examples', 'example_mtc', 'output')
-
-@pytest.fixture(scope='module')
-def trips(data_path):
-     return pd.read_csv(os.path.join(data_path, 'final_trips.csv'))
-
-@pytest.fixture(scope='module')
-def tours(data_path):
-     return pd.read_csv(os.path.join(data_path, 'final_tours.csv'))
-
-@pytest.fixture(scope='module')
-def tables(trips, tours):
-     return {
-        'trips': trips,
-        'tours': tours,
-     }
-
-# def test_summarize(tours):
-#     setup_dirs()
-#     summarize.summarize(tours)
 
 
-# @pytest.fixture(scope='module')
-# def synthesize_pipeline(tables):
-#     if not pipeline.is_open():
-#         try:
-#             # Open the pipeline from an available h5 file
-#             # (this makes sure an available h5 file isn't overwritten by a new pipeline)
-#             ipeline.open_pipeline('_')
-#         except:
-#             # Otherwise, create a new pipeline
-#             pipeline.open_pipeline()
-#     test_checkpoint = 'test_summarize'
-#     # Make a new test checkpoint if it doesn't exist already
-#     if test_checkpoint not in pipeline.get_checkpoints().checkpoint_name:
-#         pipeline.add_checkpoint('test_summarize')
-#     # Otherwise, go to that checkpoint
-#     else:
-#         pipeline.load_checkpoint('test_summarize')
-#     # Replace specified tables with inputs 
-#     for name, df in tables.items():
-#         pipeline.replace_table(name, df)
-    
-
-# def test_summarize(tables=None):
-#     setup_dirs()
-#     if tables:
-#         synthesize_pipeline(tables)
-#     else:
-#         if not pipeline.is_open():
-#             pipeline.open_pipeline('_')
-#     summarize()
-
-
-def test_summarize(tables):
-    setup_dirs()
-    summarize(tables)
+def test_summarize():
+    ### Set to return the tables as they would be after the last step in the model
+    pipeline.run(models=['summarize'], resume_after='trip_mode_choice')
 
 
 

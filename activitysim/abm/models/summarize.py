38ae31bc8441c390d8fa62d55bba37535530e04a
Clint Daniels <clint.daniels@wsp.com>
2021-12-06 22:33:52 -0800

    Reimplement initial skims based calculations. more work needed.

--- activitysim/abm/models/summarize.py
+++ activitysim/abm/models/summarize.py
@@ -13,8 +13,33 @@ from activitysim.abm.models.trip_matrices import annotate_trips
 logger = logging.getLogger(__name__)
 
 
+def wrap_skims(network_los, trips_merged):
+    skim_dict = network_los.get_default_skim_dict()
+
+    trips_merged['start_tour_period'] = network_los.skim_time_period_label(trips_merged['start'])
+    trips_merged['end_tour_period'] = network_los.skim_time_period_label(trips_merged['end'])
+    trips_merged['trip_period'] = network_los.skim_time_period_label(trips_merged['depart'])
+
+    odt_skim_stack_wrapper = skim_dict.wrap_3d(orig_key='origin_tour', dest_key='destination_tour',
+                                               dim3_key='start_tour_period').set_df(trips_merged)
+    dot_skim_stack_wrapper = skim_dict.wrap_3d(orig_key='destination_tour', dest_key='origin_tour',
+                                               dim3_key='end_tour_period').set_df(trips_merged)
+    odr_skim_stack_wrapper = skim_dict.wrap_3d(orig_key='origin_trip', dest_key='destination_trip',
+                                               dim3_key='trip_period').set_df(trips_merged)
+
+    od_skim_stack_wrapper = skim_dict.wrap('origin_tour', 'destination_tour').set_df(trips_merged)
+    od_trip_skim_stack_wrapper = skim_dict.wrap('origin_trip', 'destination_trip').set_df(trips_merged)
+
+    return {
+        "tour_odt_skims": odt_skim_stack_wrapper,
+        "tour_dot_skims": dot_skim_stack_wrapper,
+        "trip_odt_skims": odr_skim_stack_wrapper,
+        "tour_od_skims": od_skim_stack_wrapper,
+        "trip_od_skims": od_trip_skim_stack_wrapper
+    }
+
 @inject.step()
-def summarize(network_los, persons_merged):
+def summarize(network_los, persons_merged, trips, tours_merged):
     """
     summarize is a standard model which uses expression files
     to reduce tables
@@ -26,13 +51,29 @@ def summarize(network_los, persons_merged):
     output_location = model_settings['OUTPUT'] if 'OUTPUT' in model_settings  else 'summaries'
     os.makedirs(config.output_file_path(output_location), exist_ok=True)
 
-    persons_merged = persons_merged.to_frame()
+    spec = pd.read_csv(config.config_file_path(model_settings['SPECIFICATION']))
 
-    for table_name, meta in model_settings['TABLES'].items():
-        if table_name == 'persons_merged':
-            df = persons_merged
-        else:
-            df = inject.get_table(table_name).to_frame()
+    persons_merged = persons_merged.to_frame()
+    trips = trips.to_frame()
+    tours_merged = tours_merged.to_frame()
+
+    # - trips_merged - merge trips and tours_merged
+    trips_merged = pd.merge(
+        trips,
+        tours_merged.drop(columns=['person_id', 'household_id']),
+        left_on='tour_id',
+        right_index=True,
+        suffixes=['_trip', '_tour'],
+        how="left")
+
+    locals_d = {
+        'trips_merged': trips_merged,
+        'persons_merged': persons_merged
+    }
+
+    for table_name, df in locals_d.items():
+        meta = model_settings[table_name]
+        df = eval(table_name)
 
         if 'AGGREGATE' in meta and meta['AGGREGATE']:
             for agg in meta['AGGREGATE']:
@@ -51,25 +92,15 @@ def summarize(network_los, persons_merged):
 
         # locals_d['persons'] = inject.get_table('persons_merged', None).to_frame()
 
-        # Save table for testing
-        # locals_d['trips'].to_csv(config.output_file_path(os.path.join(output_location, f'trips_merged.csv')))
-        # locals_d['persons'].to_csv(config.output_file_path(os.path.join(output_location, f'persons_merged.csv')))
+    locals_d.update(wrap_skims(network_los,trips_merged))
 
-        if 'SPECIFICATION' in meta:
-            spec = pd.read_csv(config.config_file_path(meta['SPECIFICATION']))
+    for i, row in spec.iterrows():
 
-            locals_d = {
-                table_name: df,
-                'persons_merged': persons_merged
-            }
+        out_file = row['Output']
+        expr = row['Expression']
 
-            for i, row in spec.iterrows():
+        logger.info(f'Summary: {expr} -> {out_file}.csv')
 
-                out_file = row['Output']
-                expr = row['Expression']
-
-                logger.info(f'Summary: {expr} -> {out_file}.csv')
-
-                resultset = eval(expr, globals(), locals_d)
-                resultset.to_csv(config.output_file_path(os.path.join(output_location, f'{out_file}.csv')), index=False)
+        resultset = eval(expr, globals(), locals_d)
+        resultset.to_csv(config.output_file_path(os.path.join(output_location, f'{out_file}.csv')), index=False)
 

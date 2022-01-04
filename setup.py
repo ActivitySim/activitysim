5356a19cc79f7bee8e8d6a2ad665d8e06f81df81
Clint Daniels <clint.daniels@wsp.com>
2022-01-04 14:12:09 -0800

    Constrain Numpy version #533

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     entry_points={'console_scripts': ['activitysim=activitysim.cli.main:main']},
     install_requires=[
         'pyarrow >= 2.0',
-        'numpy >= 1.16.1',
+        'numpy >= 1.16.1,<=1.21',
         'openmatrix >= 0.3.4.1',
         'pandas >= 1.1.0',
         'pyyaml >= 5.1',

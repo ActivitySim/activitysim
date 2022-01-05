89013ce5456dda6faabb0da51042e6b5adaf5ecc
Chester Harvey <chesterharvey@gmail.com>
2022-01-05 10:45:24 -0700

    aggregation tests added for summarize model

--- activitysim/abm/test/test_misc/test_summarize.py
+++ activitysim/abm/test/test_misc/test_summarize.py
@@ -1,10 +1,11 @@
 import logging
-
 import pytest
+import os
+import pandas as pd
 
 # import models is necessary to initalize the model steps with orca
 from activitysim.abm import models
-from activitysim.core import pipeline
+from activitysim.core import pipeline, config
 
 
 # Used by conftest.py initialize_pipeline method
@@ -47,5 +48,21 @@ def initialize_network_los() -> bool:
 
 
 def test_summarize(initialize_pipeline: pipeline.Pipeline, caplog):
+    # Run summarize model
     caplog.set_level(logging.DEBUG)
     pipeline.run(models=['summarize'])
+    
+    # Retrieve output tables to check contents
+    model_settings = config.read_model_settings('summarize.yaml')
+    output_location = (model_settings['OUTPUT'] if 'OUTPUT' in model_settings else 'summaries')
+    output_dir = config.output_file_path(output_location)
+
+    # Check that households are counted correctly
+    households_count = pd.read_csv(config.output_file_path(os.path.join(output_location, f'households_count.csv')))
+    households = pd.read_csv(config.data_file_path("households.csv"))
+    assert int(households_count.iloc[0]) == len(households)
+
+    # Check that bike trips are counted correctly
+    trips_by_mode_count = pd.read_csv(config.output_file_path(os.path.join(output_location, f'trips_by_mode_count.csv')))
+    trips = pd.read_csv(config.data_file_path("trips.csv"))
+    assert int(trips_by_mode_count.BIKE.iloc[0]) == len(trips[trips.trip_mode == 'BIKE'])

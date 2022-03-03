b9ecce48baa69b345efe4a043810ac3477fcaf87
Sijia Wang <wangsijia0628@gmail.com>
2022-03-03 15:32:41 -0500

    Revert "Merge branch 'mtc_tm2' into ft_vis_1"
    This reverts commit d96c740d1119e04fa7dbd97cd474b5f3881d0d98, reversing
    changes made to 3974e39e68a49006ae48527fb1aa81b5af0234ed.

--- docker/entrypoint_debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# run tests in live debug
-source ../../opt/conda/etc/profile.d/conda.sh
-conda activate ASIM-DEV
-python -m debugpy --listen 0.0.0.0:5678 --wait-for-client ../../opt/conda/envs/ASIM-DEV/bin/pytest activitysim/abm/test/test_misc/test_summarize.py

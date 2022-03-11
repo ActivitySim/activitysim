d3dcb8035e02ba0522cfbb8fdfdd8fc294eb0c36
Jeff Newman <jeffnewman@camsys.com>
2022-03-11 13:30:47 -0600

    thread stopper when invoking activitysim1

---
+++ activitysim/__main__.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+def main():
+    # clean up message formatting
+    if sys.argv and sys.argv[0].endswith('__main__.py'):
+        sys.argv[0] = 'activitysim'
+
+    # threadstopper
+    os.environ['MKL_NUM_THREADS'] = '1'
+    os.environ['OMP_NUM_THREADS'] = '1'
+    os.environ['OPENBLAS_NUM_THREADS'] = '1'
+    os.environ['NUMBA_NUM_THREADS'] = '1'
+    os.environ['VECLIB_MAXIMUM_THREADS'] = '1'
+    os.environ['NUMEXPR_NUM_THREADS'] = '1'
+
+    from .cli.main import main
+    main()
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -25,7 +25,10 @@ setup(
     ],
     packages=find_packages(exclude=['*.tests']),
     include_package_data=True,
-    entry_points={'console_scripts': ['activitysim=activitysim.cli.main:main']},
+    entry_points={'console_scripts': [
+        'activitysim=activitysim.cli.main:main',
+        'activitysim1=activitysim.__main__:main',
+    ]},
     install_requires=[
         'pyarrow >= 2.0',
         'numpy >= 1.16.1',

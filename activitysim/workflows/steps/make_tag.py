25b46a0755bb5960c0d75c5f565334f75b9e8d29
Jeff Newman <jeffnewman@camsys.com>
2022-06-22 16:26:10 -0500

    sandag-1zone-complete

--- activitysim/workflows/steps/make_tag.py
+++ activitysim/workflows/steps/make_tag.py
@@ -17,6 +17,8 @@ def make_tag(
     reference=None,
     reference_asim_version="0.0.0",
     multiprocess=0,
+    chunk_training_mode=None,
+    main_n_households=None,
 ):
     reset_progress_step(description="Initialize Tag")
     if tag is None:
@@ -44,4 +46,10 @@ def make_tag(
     out["relabel_tablesets"] = {"reference": f"v{reference_asim_version}"}
     out["is_multiprocess"] = multiprocess > 1
     out["num_processes"] = int(multiprocess)
+
+    if chunk_training_mode and chunk_training_mode != 'disabled':
+        out["chunk_application_mode"] = "production"
+    else:
+        out["chunk_application_mode"] = chunk_training_mode
+
     return out

120c7604b94de92f5a8316bcfff6dfe5a9dbadb7
Nick Fournier <nick.fournier@rsginc.com>
2022-09-12 10:52:50 -0700

    began setup for multiprocessing

--- activitysim/examples/prototype_mtc_accessibilities/disaggregate_accessibility_model.py
+++ activitysim/examples/prototype_mtc_accessibilities/disaggregate_accessibility_model.py
@@ -1,8 +1,9 @@
 import argparse
 import os
 import pkg_resources
+import sys
 
-from activitysim.cli.run import add_run_args
+from activitysim.cli.run import add_run_args, run
 from activitysim.abm.models.disaggregate_accessibility_run import run_disaggregate_accessibility
 
 
@@ -24,12 +25,13 @@ def disaggregate_accessibility():
     args = parser.parse_args()
 
     # add in the arguments
-    # args.config = [extended_path('configs'), base_path('configs')]
-    args.config = [extended_path('configs_mp'), extended_path('configs'), base_path('configs')]
+    args.config = [extended_path('configs'), base_path('configs')]
+    args.config.insert(0, extended_path('configs_mp'))
     args.output = extended_path('output')
     args.data = base_path('data')
 
-    run_disaggregate_accessibility(args)
+    # run_disaggregate_accessibility(args)
+    sys.exit(run(args))
 
 if __name__ == "__main__":
     disaggregate_accessibility()

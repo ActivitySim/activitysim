d9c465c9e2e1d3a330d89a75f25e9017b092ce30
Nick Fournier <nick.fournier@rsginc.com>
2022-09-30 16:54:33 -0700

    fixed conflict with persons_merged

--- activitysim/abm/tables/disaggregate_accessibility.py
+++ activitysim/abm/tables/disaggregate_accessibility.py
@@ -16,11 +16,12 @@ from activitysim.core import (inject,
 logger = logging.getLogger(__name__)
 
 
-def find_nearest_accessibility_zone(choosers, accessibility_df, origin_col='home_zone_id', method='skims'):
+def find_nearest_accessibility_zone(choosers, accessibility_df, method='skims'):
     """
     Matches choosers zone to the nearest accessibility zones.
     Can be achieved by querying the skims or by nearest neighbor of centroids
     """
+    origin_col = 'home_zone_id'
 
     def weighted_average(df, values, weights):
         return df[values].T.dot(df[weights]) / df[weights].sum()
@@ -38,6 +39,9 @@ def find_nearest_accessibility_zone(choosers, accessibility_df, origin_col='home
     # First find any choosers zones that are missing from accessibility zones
     matched_zones = list(set(unique_origin_zones).intersection(accessibility_zones))
     unmatched_zones = list(set(unique_origin_zones).difference(accessibility_zones))
+
+    # Store choosers index to ensure consistency
+    _idx = choosers.index
 
     if method == 'centroids':
         # Extract and vectorize TAZ centroids
@@ -69,7 +73,7 @@ def find_nearest_accessibility_zone(choosers, accessibility_df, origin_col='home
     matched_df = choosers.reset_index().merge(matched_df, on=origin_col)
     matched_df = matched_df.set_index(choosers.index.name)
 
-    return matched_df
+    return matched_df.loc[_idx]
 
 
 @inject.table()
@@ -122,54 +126,45 @@ def initialize_disaggregate_accessibility():
         return
 
     # Extract model settings
+    config.read_model_settings('disaggregate_accessibility.yaml')
+
     model_settings = config.read_model_settings('disaggregate_accessibility.yaml')
     merging_params = model_settings.get('MERGE_ON')
-    origin_col = merging_params.get('origin_col', 'home_zone_id')
-    logsums_cols = [x for x in proto_accessibility_df.columns if 'logsums' in x]
+    accessibility_cols = [x for x in proto_accessibility_df.columns if 'accessibility' in x]
 
     # Parse the merging parameters
     assert merging_params is not None
 
     # Check if already assigned!
-    if all([True for x in logsums_cols if x in persons_merged_df.columns]):
+    if set(accessibility_cols).intersection(persons_merged_df.columns) == set(accessibility_cols):
         return
 
     # Find the nearest zone (spatially) with accessibilities calculated
+    # Note that from here on the 'home_zone_id' is the matched name
     if 'nearest_accessibility_zone_id' not in persons_merged_df.columns:
-        persons_merged_df = find_nearest_accessibility_zone(persons_merged_df, proto_accessibility_df, origin_col)
+        persons_merged_df = find_nearest_accessibility_zone(persons_merged_df, proto_accessibility_df)
 
-    # Rename the home_zone_id in proto-table to match the temporary 'nearest_zone_id'
-    proto_accessibility_df['nearest_accessibility_zone_id'] = proto_accessibility_df[origin_col]
+    # Copy home_zone_id in proto-table to match the temporary 'nearest_zone_id'
+    proto_accessibility_df['nearest_accessibility_zone_id'] = proto_accessibility_df.home_zone_id
 
     # Set up the useful columns
-    exact_cols = ['nearest_accessibility_zone_id'] + merging_params.get('by', [])
+    exact_cols = merging_params.get('by', [])
+    if 'home_zone_id' in exact_cols:
+        exact_cols.remove('home_zone_id')
+    exact_cols.insert(0, 'nearest_accessibility_zone_id')
+
     nearest_cols = merging_params.get('asof', [])
     merge_cols = exact_cols + nearest_cols
 
-    # Perform an ASOF join, which first matches exact values, then finds the "nearest" values
-    right_df = proto_accessibility_df[merge_cols + logsums_cols].sort_values(nearest_cols)
+    assert len(nearest_cols) <= 1
+
+    # Setup and left and right tables. If asof join is used, it must be sorted.
+    # Drop duplicate accessibilities once filtered (may expect duplicates on households)
+    right_df = proto_accessibility_df[merge_cols + accessibility_cols].sort_values(nearest_cols).drop_duplicates()
     left_df = persons_merged_df[merge_cols].sort_values(nearest_cols)
 
-    if merging_params.get('method') == 'hard':
-        # merge_asof is sensitive to dataframe data types. Ensure consistency by 'upgrading' any int32 to int64
-        for col in merge_cols:
-            if left_df[col].dtype is not right_df[col].dtype:
-                assert ptypes.is_integer_dtype(left_df[col]) and ptypes.is_integer_dtype(right_df[col])
-                datatype = np.max([left_df[col].dtype, right_df[col].dtype])
-                left_df[col] = left_df[col].astype(datatype)
-                right_df[col] = right_df[col].astype(datatype)
-
-        if nearest_cols:
-            merge_df = pd.merge_asof(left=left_df, right=right_df, by=exact_cols, on=nearest_cols, direction='nearest')
-            merge_df = merge_df.set_index('person_id')
-        else:
-            merge_df = pd.merge(left=left_df.reset_index(), right=right_df, on=exact_cols, how='left')
-
-    else:
-        # A little worried that full model is too big, may need segment on zone
-        # for zone in persons_merged_df[origin_col].unique():
-        # x_pop = left_df[left_df.nearest_accessibility_zone_id==zone][merge_cols[1:]]
-        # x_proto = right_df[right_df.nearest_accessibility_zone_id == zone][merge_cols[1:]]
+    if merging_params.get('method') == 'soft':
+        # a 'soft' merge is possible by finding the nearest neighbor
         x_pop, x_proto = left_df[exact_cols], right_df[exact_cols]
         y = x_proto.index
 
@@ -187,20 +182,32 @@ def initialize_disaggregate_accessibility():
               ' {}% (<100% typically means insufficient merge-on features.)'.format(accuracy))
 
         # Predict the nearest person ID and pull the logsums
-        matched_logsums_df = right_df.loc[clf.predict(x_pop)][logsums_cols].reset_index(drop=True)
+        matched_logsums_df = right_df.loc[clf.predict(x_pop)][accessibility_cols].reset_index(drop=True)
         merge_df = pd.concat([left_df.reset_index(drop=False), matched_logsums_df], axis=1).set_index('person_id')
+
+    else:
+        # merge_asof is sensitive to dataframe data types. Ensure consistency by 'upgrading' any int32 to int64
+        for col in merge_cols:
+            if left_df[col].dtype is not right_df[col].dtype:
+                assert ptypes.is_integer_dtype(left_df[col]) and ptypes.is_integer_dtype(right_df[col])
+                datatype = np.max([left_df[col].dtype, right_df[col].dtype])
+                left_df[col] = left_df[col].astype(datatype)
+                right_df[col] = right_df[col].astype(datatype)
+
+        if nearest_cols:
+            merge_df = pd.merge_asof(left=left_df, right=right_df, by=exact_cols, on=nearest_cols, direction='nearest')
+        else:
+            merge_df = pd.merge(left=left_df.reset_index(), right=right_df, on=exact_cols, how='left')
+        merge_df = merge_df.set_index('person_id')
 
     # Check that it was correctly left-joined
     assert persons_merged_df[merge_cols].sort_index().equals(merge_df[merge_cols].sort_index())
-
-    for col in logsums_cols:
-        if merge_df[col].isnull().values.any():
-            print("WARNING: NaNs found in disaggregate accessibilities '{}'".format(col))
+    assert any(merge_df[accessibility_cols].isnull())
 
     # Drop the temporary ID zone?
     # persons_merged_df.drop(columns='nearest_accessibility_zone_id')
 
     # Merge the accessibilities to the persons_merged table and update the pipeline
-    pipeline.replace_table('persons_merged', persons_merged_df.join(merge_df[logsums_cols]))
+    pipeline.replace_table('persons', persons_merged_df.join(merge_df[accessibility_cols]))
 
     return

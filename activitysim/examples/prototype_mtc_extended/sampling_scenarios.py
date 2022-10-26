17c6b21565eeffe501b71979065729db66f682fa
Nick Fournier <nick.fournier@rsginc.com>
2022-10-26 13:25:01 -0700

    more flexible scenario testing

--- activitysim/examples/prototype_mtc_extended/sampling_scenarios.py
+++ activitysim/examples/prototype_mtc_extended/sampling_scenarios.py
@@ -1,5 +1,4 @@
 import argparse
-from genericpath import isfile
 import os
 import pkg_resources
 import yaml
@@ -8,21 +7,31 @@ import shutil
 from activitysim.cli.run import add_run_args, run
 from activitysim.core.util import named_product
 
-
+START_ITER = 0
 SAMPLING_PARAMS = {
     "DESTINATION_SAMPLE_SIZE": [0.1, 1 / 3, 2 / 3, 0],
     "ORIGIN_SAMPLE_SIZE": [0.1, 1 / 3, 2 / 3, 0],
     "ORIGIN_SAMPLE_METHOD": [None],  # , 'kmeans']
 }
 
+def integer_params(params):
+    n_zones = count_lines_enumerate(base_path("data_2/land_use.csv"))
+
+    d_zones = 1 if params.DESTINATION_SAMPLE_SIZE > 1 else n_zones
+    o_zones = 1 if params.ORIGIN_SAMPLE_SIZE > 1 else n_zones
+
+    params.DESTINATION_SAMPLE_SIZE = round(params.DESTINATION_SAMPLE_SIZE * d_zones)
+    params.ORIGIN_SAMPLE_SIZE = round(params.ORIGIN_SAMPLE_SIZE * o_zones)
+
+    return params
 
 def base_path(dirname):
-    resource = os.path.join("examples", "prototype_mtc", dirname)
+    resource = os.path.join("examples", "placeholder_sandag_2_zone", dirname)
     return pkg_resources.resource_filename("activitysim", resource)
 
 
 def extended_path(dirname):
-    resource = os.path.join("examples", "prototype_mtc_extended", dirname)
+    resource = os.path.join("examples", "placeholder_sandag_2_zone_extended", dirname)
     return pkg_resources.resource_filename("activitysim", resource)
 
 
@@ -35,10 +44,11 @@ def run_model():
     args.config = [
         extended_path("configs_mp"),
         extended_path("configs"),
-        base_path("configs"),
+        base_path("configs_2_zone"),
+        base_path("placeholder_psrc/configs"),
     ]
     args.output = extended_path("output")
-    args.data = base_path("data")
+    args.data = [extended_path("data"), base_path("data_2")]
     run(args)
 
 
@@ -49,43 +59,47 @@ def count_lines_enumerate(file_name):
 
 
 def update_configs(scene, model_settings, config_path):
-    n_zones = count_lines_enumerate(base_path("data/land_use.csv"))
-
-    d_zones = 1 if scene.DESTINATION_SAMPLE_SIZE > 1 else n_zones
-    o_zones = 1 if scene.ORIGIN_SAMPLE_SIZE > 1 else n_zones
-
-    d_size = round(scene.DESTINATION_SAMPLE_SIZE * d_zones)
-    o_size = round(scene.ORIGIN_SAMPLE_SIZE * o_zones)
-    method = scene.ORIGIN_SAMPLE_METHOD
-
     # Update the model settings
-    model_settings["DESTINATION_SAMPLE_SIZE"] = d_size
-    model_settings["ORIGIN_SAMPLE_SIZE"] = o_size
-    model_settings["ORIGIN_SAMPLE_METHOD"] = method
+    scene = integer_params(scene)
+    model_settings["DESTINATION_SAMPLE_SIZE"] = scene.DESTINATION_SAMPLE_SIZE
+    model_settings["ORIGIN_SAMPLE_SIZE"] = scene.ORIGIN_SAMPLE_SIZE
+    model_settings["ORIGIN_SAMPLE_METHOD"] = scene.ORIGIN_SAMPLE_METHOD
 
     with open(config_path, "w") as file:
         yaml.dump(model_settings, file)
 
     return model_settings
 
+def make_scene_name(it, params):
+    d_samp = params["DESTINATION_SAMPLE_SIZE"]
+    o_samp = params["ORIGIN_SAMPLE_SIZE"]
+    method = params["ORIGIN_SAMPLE_METHOD"]
 
-def copy_output(iter, model_settings):
-    scene_dir_name = "scenarios_output/scene-{}_dsamp-{}_osamp-{}_method-{}".format(
-        iter,
-        model_settings["DESTINATION_SAMPLE_SIZE"],
-        model_settings["ORIGIN_SAMPLE_SIZE"],
-        model_settings["ORIGIN_SAMPLE_METHOD"],
+    scene_name = "scene-{}_dsamp-{}_osamp-{}_method-{}".format(
+        it + START_ITER, d_samp, o_samp, method,
     )
+
+    return scene_name
+
+
+def copy_output(scene_name, model_settings):
+
+    scene_dir_name = os.path.join('scenarios_output', scene_name)
 
     if os.path.exists(extended_path(scene_dir_name)):
         shutil.rmtree(extended_path(scene_dir_name))
     os.makedirs(extended_path(scene_dir_name))
 
-    files_list = [
-        x
-        for x in os.listdir(extended_path("output"))
-        if "pipeline" not in x and "cache" not in x
-    ]
+    log = pd.read_csv('scenarios_output/log.csv')
+    filt = (
+            (log.DESTINATION_SAMPLE_SIZE == model_settings["DESTINATION_SAMPLE_SIZE"]) &
+            (log.ORIGIN_SAMPLE_SIZE == model_settings["ORIGIN_SAMPLE_SIZE"]) &
+            (log.ORIGIN_SAMPLE_METHOD == model_settings["ORIGIN_SAMPLE_METHOD"])
+    )
+    log.loc[filt, 'COMPLETED_ID'] = scene_name
+    log.to_csv('scenarios_output/log.csv', index=False)
+
+    files_list = [x for x in os.listdir(extended_path('output')) if 'pipeline' not in x and 'cache' not in x]
 
     for file in files_list:
         copyargs = {
@@ -101,18 +115,41 @@ def copy_output(iter, model_settings):
     return
 
 
+
 def run_scenarios():
     config_path = extended_path("configs/disaggregate_accessibility.yaml")
     with open(config_path) as file:
         model_settings = yaml.load(file, Loader=yaml.FullLoader)
 
-    scenarios = pd.DataFrame(named_product(**SAMPLING_PARAMS))
-    for iter, scene in scenarios.iterrows():
+    if not os.path.exists(extended_path('scenarios_output')):
+        os.makedirs(extended_path('scenarios_output'))
+
+    if os.path.exists(extended_path('scenarios_output/log.csv')):
+        log = pd.read_csv(extended_path('scenarios_output/log.csv'))
+        # assert scenarios[['DESTINATION_SAMPLE_SIZE','ORIGIN_SAMPLE_SIZE', 'ORIGIN_SAMPLE_METHOD']].equals(
+        #     log[['DESTINATION_SAMPLE_SIZE','ORIGIN_SAMPLE_SIZE', 'ORIGIN_SAMPLE_METHOD']]
+        # )
+        scenarios = log
+    else:
+        scenarios = pd.DataFrame(named_product(**SAMPLING_PARAMS))
+        scenarios['COMPLETED_ID'] = ""
+        scenarios['SKIP'] = False
+        scenarios.to_csv(extended_path('scenarios_output/log.csv'), index=False)
+
+    for it, scene in scenarios.iterrows():
         # Update model settings
         model_settings = update_configs(scene, model_settings, config_path)
+
+        # Check if already run
+        scene_name = make_scene_name(it, scene)
+        scene_dir_name = extended_path(os.path.join('scenarios_output', scene_name))
+
+        if any(scenarios.COMPLETED_ID == scene_name) and os.path.exists(scene_dir_name) or scene.SKIP:
+            continue
+
         # Run the model
         print(
-            f"Running model {iter} of {len(scenarios.index)}: {chr(10)}"
+            f"Running model {it} of {len(scenarios.index)}: {chr(10)}"
             + f"{chr(10)}".join(
                 [f"{var}={model_settings[var]}" for var in SAMPLING_PARAMS.keys()]
             )
@@ -120,9 +157,9 @@ def run_scenarios():
         try:
             run_model()
             # Copy results to named folder
-            copy_output(iter, model_settings)
+            copy_output(scene_name, model_settings)
         except:
-            print(f"Failed on scene {iter}")
+            print(f"Failed on scene {scene_name}")
 
 
 if __name__ == "__main__":

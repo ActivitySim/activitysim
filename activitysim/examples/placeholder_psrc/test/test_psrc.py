2be376a10f3bf7a4784c3f4e11ccdf1b09914e39
Jeff Newman <jeffnewman@camsys.com>
2023-03-05 10:56:13 -0600

    fix tests

--- activitysim/examples/placeholder_psrc/test/test_psrc.py
+++ activitysim/examples/placeholder_psrc/test/test_psrc.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 # ActivitySim
 # See full license in LICENSE.txt.
 import os
@@ -7,6 +9,8 @@ import sys
 import pandas as pd
 import pandas.testing as pdt
 import pkg_resources
+
+from activitysim.core import testing
 
 
 def _test_psrc(sharrow=False):
@@ -24,7 +28,7 @@ def _test_psrc(sharrow=False):
         # person_id,household_id,tour_id,primary_purpose,trip_num,outbound,trip_count,purpose,
         # destination,origin,destination_logsum,depart,trip_mode,mode_choice_logsum
         # compare_cols = []
-        pdt.assert_frame_equal(final_trips_df, regress_trips_df)
+        testing.assert_frame_substantively_equal(final_trips_df, regress_trips_df)
 
     file_path = os.path.join(os.path.dirname(__file__), "simulation.py")
 

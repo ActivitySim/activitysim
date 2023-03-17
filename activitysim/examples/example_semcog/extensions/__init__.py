0a741d1a64b5c69ee324cd7155d58dc1acf52c83
Ali Etezady <58451076+aletzdy@users.noreply.github.com>
2023-03-17 16:55:23 -0400

    updated configs

--- activitysim/examples/example_semcog/extensions/__init__.py
+++ activitysim/examples/example_semcog/extensions/__init__.py
@@ -1,7 +1,3 @@
-from . import work_from_home
-from . import telecommute_frequency
 from . import parking_location_choice_at_university
 from . import stop_frequency_university_parking
 from . import university_location_zone_override
-from . import transit_pass_ownership
-from . import transit_pass_subsidy

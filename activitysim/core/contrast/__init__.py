ca760ce60321eec5a3843762af38a0cb59e3946f
Jeff Newman <jeff@newman.me>
2023-07-14 11:36:47 -0500

    drop core.contrast from this PR

--- activitysim/core/contrast/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Tools for contrasting the data or processes of various ActivitySim States.
-"""
-
-from __future__ import annotations
-
-from ._optional import altair
-from .continuous import compare_histogram
-from .nominal import NominalTarget, compare_nominal

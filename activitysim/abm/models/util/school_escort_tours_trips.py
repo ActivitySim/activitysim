c4394056409f22b5f916fd8da2ba4088f9ee6185
David Hensle <hensle93@gmail.com>
2024-03-18 10:05:23 -0700

    blacken

--- activitysim/abm/models/util/school_escort_tours_trips.py
+++ activitysim/abm/models/util/school_escort_tours_trips.py
@@ -408,7 +408,13 @@ def merge_school_escort_trips_into_pipeline(state: workflow.State):
     # checking to see if there are school escort trips to merge in
     if len(school_escort_trips) == 0:
         # if no trips, fill escorting columns with NA
-        trips[["escort_participants", "school_escort_direction", "school_escort_trip_id",]] = pd.NA
+        trips[
+            [
+                "escort_participants",
+                "school_escort_direction",
+                "school_escort_trip_id",
+            ]
+        ] = pd.NA
         state.replace_table("trips", trips)
         return trips
 

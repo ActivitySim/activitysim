367a8b6e5fadbae99599870769c37b0cc1885e0f
jpn-- <jpn--@users.noreply.github.com>
2024-04-04 14:29:18 +0000

    deploy: 739c2e428856eb401b42e95cedf9fd88bfcb02c6

--- develop/_static/documentation_options.js
+++ develop/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.3.0b2.dev60+gbb437b34',
+    VERSION: '1.3.0b2.dev61+g739c2e42',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

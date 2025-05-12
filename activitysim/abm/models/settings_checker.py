aa2031bff6060739af590384edf1beffff132238
Andrew Kay <64652494+andkay@users.noreply.github.com>
2025-05-12 11:12:14 -0400

    refactor: refactors setting checking to separate read/evaluate. checks now return result of tuple (Dataframe, Exception | None) for collection of errors.

--- activitysim/abm/models/settings_checker.py
+++ activitysim/abm/models/settings_checker.py
@@ -1,7 +1,8 @@
 import logging
+import os
 from pandas import DataFrame
 from pydantic import BaseModel as PydanticBase
-from typing import Type
+from typing import Type, Optional
 
 from activitysim.core.workflow import State
 from activitysim.core.simulate import eval_coefficients, eval_nest_coefficients
@@ -56,6 +57,12 @@ from activitysim.core.configuration.logit import (
 
 logger = logging.getLogger(__name__)
 file_logger = logger.getChild("logfile")
+
+class SettingsFileError(Exception):
+    """Only used to raise descriptive error, not convey error details."""
+    def __init__(self):
+        super().__init__(self)
+
 
 COMPONENTS_TO_SETTINGS = {
     "compute_accessibility": {
@@ -226,99 +233,170 @@ def try_load_model_settings(
     model_settings_class: Type[PydanticBase],
     model_settings_file: str,
     state: State,
-) -> PydanticBase:
+) -> tuple:
     logger.info(
         f"Attempting to load model settings for {model_name} via {model_settings_class.__name__} and {model_settings_file}"
     )
-    if isinstance(model_settings_class, DisaggregateAccessibilitySettings):
-        settings = read_disaggregate_accessibility_yaml(state, model_settings_file)
-    else:
-        settings = model_settings_class.read_settings_file(
-            state.filesystem, model_settings_file
-        )
-    logger.info(f"Successfully loaded model settings from {model_settings_file}")
-    return settings
+    try:
+        if isinstance(model_settings_class, DisaggregateAccessibilitySettings):
+            model_settings = read_disaggregate_accessibility_yaml(state, model_settings_file)
+        else:
+            model_settings = model_settings_class.read_settings_file(
+                state.filesystem, model_settings_file
+            )
+        result = model_settings, None
+        logger.info(f"Successfully loaded model settings from {model_settings_file}")
+    except Exception as e:
+        result = None, e
+    return result
 
 
 def try_load_spec(
-    model_name: str, model_settings: PydanticBase, state: State
-) -> DataFrame:
-
+    model_name: str, 
+    model_settings: PydanticBase, 
+    spec_file: str, 
+    state: State
+) -> tuple[DataFrame, Optional[Exception]]:
     logger.info(
         f"Attempting to load SPEC for {model_name} via {model_settings.__class__.__name__}"
     )
-    spec_file = model_settings.model_dump().get("SPEC")
-    if spec_file is None:
-        logger.info(
-            f"No SPEC file is associated with {model_settings.__class__.__name__}"
-        )
-        # Need to return object with .copy() interface for eval_coefficients
-        return DataFrame()
-    spec = state.filesystem.read_model_spec(spec_file)
-    logger.info(f"Successfully loaded model SPEC from {spec_file}")
-    return spec
+    try:
+        result = state.filesystem.read_model_spec(spec_file), None
+        logger.info(f"Successfully loaded model SPEC from {spec_file}")
+    except Exception as e:
+        # always return a dataframe
+        result = DataFrame(), None
+        raise e
+    return result
 
-
-def try_load_and_eval_coefs(
-    model_name: str, model_settings: PydanticBase, spec: DataFrame, state: State
-) -> tuple[DataFrame | None, DataFrame | None]:
+def try_load_coefs(
+    model_name: str, 
+    model_settings: PydanticBase, 
+    coefs_file: str, 
+    state: State
+) -> tuple[DataFrame, Optional[Exception]]:
     logger.info(
-        f"Attempting to load coefficients for {model_name} via {model_settings.__class__.__name__}"
+        f"Attempting to load COEFFICIENTS for {model_name} via {model_settings.__class__.__name__}"
     )
-    # Assumes that if COEFFICIENTS is required, will be validated by Pydantic
-    if model_settings.model_dump().get("COEFFICIENTS"):
-        coefs_file = model_settings.COEFFICIENTS
-        coefs = state.filesystem.read_model_coefficients(model_settings)
-        # check whether coefficients should be evaluated as NESTS or not
-        if model_settings.model_dump().get("NESTS"):
-            # Proper Trace label is probably unneeded here
-            # NOTE: is this the correct way to invoke this?
-            eval_coefs = eval_nest_coefficients(
-                model_settings.NESTS, coefs, trace_label=None
-            )
-        else:
-            eval_coefs = eval_coefficients(state, spec, coefs, estimator=None)
-        logger.info(f"Successfully read and evaluated coefficients from {coefs_file}")
-        return coefs, eval_coefs
-    else:
-        logger.info(
-            f"No coefficients file is associated with {model_settings.__class__.__name__}"
-        )
-    return None, None
-
+    try:
+        result = state.filesystem.read_model_coefficients(model_settings), None
+        logger.info(f"Successfully loaded model Coefficients from {coefs_file}")
+    except Exception as e:
+        result = DataFrame(), e
+        raise e
+    return result
+    
+def try_eval_spec_coefs(
+    model_name: str, 
+    model_settings: PydanticBase, 
+    spec: DataFrame,
+    coefs: DataFrame,
+    state: State
+) -> tuple[DataFrame, Optional[Exception]]:
+        result = dict()
+        try:
+            # check whether coefficients should be evaluated as NESTS or not
+            if model_settings.model_dump().get("NESTS"):
+                # Proper Trace label is probably unneeded here
+                # TODO: Verify that this method is correct
+                result = eval_nest_coefficients(
+                    model_settings.NESTS, coefs, trace_label=None
+                ), None
+            else:
+                result = eval_coefficients(state, spec, coefs, estimator=None), None
+            logger.info(f"Successfully evaluated coefficients")
+        except Exception as e:
+            result = DataFrame(), e
+            raise e
+        return result
 
 def try_load_and_check(
     model_name: str,
     model_settings_class: Type[PydanticBase],
     model_settings_file: str,
     state: State,
-) -> None:
-
+) -> list[Exception]:
+    # collect all errors
+    errors = []
     # first, attempt to load settings
-    settings = try_load_model_settings(
+    model_settings, model_settings_error = try_load_model_settings(
         model_name=model_name,
         model_settings_class=model_settings_class,
         model_settings_file=model_settings_file,
         state=state,
     )
 
-    # then, attempt to read SPEC file
-    spec = try_load_spec(model_name=model_name, model_settings=settings, state=state)
+    if model_settings_error is not None:
+        errors.append(model_settings_error)
+        # without valid model settings, cannot 
+        return
 
-    # then, attempt to read and evaluate coefficients
-    coefs, eval_coefs = try_load_and_eval_coefs(
-        model_name=model_name, model_settings=settings, spec=spec, state=state
-    )
+    # then, attempt to read SPEC file
+    # only checks against the SPEC attr at top level of model.
+    # checking specification files keyed against arbtrary attrs
+    # is not currently supported - e.g. SchoolEscortingSettings: INBOUND_SPEC
+    spec_file = model_settings.model_dump().get("SPEC")
+    if spec_file:
+        spec, spec_error = try_load_spec(
+            model_name=model_name, 
+            model_settings=model_settings,
+            spec_file=spec_file,
+            state=state
+        )
+    else:
+        spec, spec_error = DataFrame(), None
+        logger.info(
+            f"No SPEC file is associated with {model_settings.__class__.__name__}"
+        )
+    
+    if spec_error is not None:
+        errors.append(spec_error)
+
+
+    # then attempt to read coefficients
+    coefs_file = model_settings.model_dump().get("COEFFICIENTS")
+    if coefs_file:
+        coefs, coefs_error = try_load_coefs(
+            model_name=model_name,
+            model_settings=model_settings,
+            coefs_file=coefs_file,
+            state=state
+        )
+    else:
+        coefs, coefs_error = DataFrame(), None
+        logger.info(f"No coefficients file is associated with {model_settings.__class__.__name__}")
+
+    if coefs_error is not None:
+        errors.append(coefs_error)
+
+    # then attempt to evaluate coefficients against spec
+    if not coefs.empty:
+        eval_coefs, eval_coefs_error = try_eval_spec_coefs(
+            model_name=model_name,
+            model_settings=model_settings,
+            spec=spec,
+            coefs=coefs,
+            state=state
+        )
+    else:
+        eval_coefs, eval_coefs_error = DataFrame(), None
+
+    if eval_coefs_error is not None:
+        errors.append(eval_coefs_error)
 
     # then, check preprocessors if any
     # for now, check is limited to check that the SPEC file is loadable
-    if settings.model_dump().get("preprocessor"):
-        preprocessor_settings = settings.preprocessor
+    if model_settings.model_dump().get("preprocessor"):
+        preprocessor_settings = model_settings.preprocessor
+        spec_file = preprocessor_settings.SPEC
         spec = try_load_spec(
             model_name=model_name + ": preprocessor",
             model_settings=preprocessor_settings,
-            state=state,
+            spec_file = spec_file,
+            state=state
         )
+    
+    return errors
 
 
 def check_model_settings(state: State) -> None:
@@ -336,11 +414,17 @@ def check_model_settings(state: State) -> None:
         # first, attempt to load the model settings
         settings_cls = COMPONENTS_TO_SETTINGS[c]["settings_cls"]
         settings_file = COMPONENTS_TO_SETTINGS[c]["settings_file"]
-        try_load_and_check(
+        errors = try_load_and_check(
             model_name=c,
             model_settings_class=settings_cls,
             model_settings_file=settings_file,
             state=state,
         )
 
-    print("Settings Checker Complete")
+        if len(errors) > 0:
+            logger.warning("Settings Checker Failed with the following errors")
+            for e in errors:
+                logger.error(f"\t{e}")
+            raise SettingsFileError
+
+    logging.info("Settings Checker Complete!")

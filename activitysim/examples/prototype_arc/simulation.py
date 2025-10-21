84ac97bc5d61f86d95fdaa338c87fdb8f5c26170
David Hensle <hensle93@gmail.com>
2025-10-21 11:34:54 -0700

    more file cleanup

---
+++ activitysim/examples/prototype_arc/simulation.py
@@ -0,0 +1,19 @@
+# ActivitySim
+# See full license in LICENSE.txt.
+
+# ActivitySim
+# See full license in LICENSE.txt.
+
+from __future__ import annotations
+
+import argparse
+import sys
+
+from activitysim.cli.run import add_run_args, run
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    add_run_args(parser)
+    args = parser.parse_args()
+
+    sys.exit(run(args))

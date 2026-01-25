f04bf1459d829bbea963e5ab653cf3f05928811e
Sijia Wang <wangsijia0628@gmail.com>
2026-01-25 17:05:56 -0500

    update test

--- test/skip_failed_choices/test_skip_failed_choices.py
+++ test/skip_failed_choices/test_skip_failed_choices.py
@@ -33,13 +33,11 @@ if not os.path.exists(new_configs_dir):
 def update_settings(settings_file, key, value):
     with open(settings_file, "r") as f:
         settings = yaml.safe_load(f)
-        f.close()
 
     settings[key] = value
 
     with open(settings_file, "w") as f:
         yaml.safe_dump(settings, f)
-        f.close()
 
 
 def update_uec_csv(uec_file, expression, coef_value):
@@ -95,27 +93,68 @@ def state():
     return state
 
 
-def test_skip_failed_choices(state):
+def test_skip_failed_choices_low_threshold_prototype_mtc(state):
 
     # check that the setting is indeed set to True
     assert state.settings.skip_failed_choices is True
 
-    state.run(models=state.settings.models, resume_after=None)
+    # set the threshold to 1% of households are allowed to be skipped
+    state.settings.fraction_of_failed_choices_allowed = 0.01
+
+    # the run will fail because more than 1% households will be skipped
+    with pytest.raises(RuntimeError) as excinfo:
+        state.run(models=state.settings.models, resume_after=None)
+    assert "exceeds the allowed threshold of" in str(excinfo.value)
+
+
+def test_skip_failed_choices_high_threshold_prototype_mtc(state):
+
+    # check that the setting is indeed set to True
+    assert state.settings.skip_failed_choices is True
+
+    # set the threshold to 100% of households are allowed to be skipped
+    state.settings.fraction_of_failed_choices_allowed = 1.0
+
+    # state.run(models=state.settings.models, resume_after=None)
+
+    for model in state.settings.models:
+        state.run.by_name(model)
+        skipped_household_ids_dict = state.get("skipped_household_ids", dict())
+        # get the keys of the dict
+        skipped_household_ids_keys = skipped_household_ids_dict.keys()
+        if model == "initialize_households":
+            # check households with home zone if 7 and 18 are in the simulated households
+            assert 7 in state.get("households").loc[:, "home_zone_id"].values
+            assert 18 in state.get("households").loc[:, "home_zone_id"].values
+        if model == "auto_ownership_simulate":
+            # assert one of the keys contains "auto_ownership_simulate"
+            assert any(
+                "auto_ownership_simulate" in key for key in skipped_household_ids_keys
+            )
+            # assert that the number of skipped households for auto_ownership_simulate is 598
+            assert state.get("num_skipped_households", 0) == (69 + 598)
+            # check no households in home zone 8 (recoded 7) in the remaining households
+            assert all(state.get("households").loc[:, "home_zone_id"] != 7)
+            assert all(state.get("persons").loc[:, "home_zone_id"] != 7)
+        elif model == "workplace_location":
+            # assert one of the keys contains "workplace_location"
+            assert any(
+                "workplace_location" in key for key in skipped_household_ids_keys
+            )
+            # assert that the number of skipped households for workplace_location is 69
+            assert state.get("num_skipped_households", 0) == 69
+            # assert no workers from home zone 18 in the remaining workers
+            persons_df = state.get_dataframe("persons")
+            assert not any(
+                (persons_df["home_zone_id"] == 18) & (persons_df["is_worker"] == True)
+            )
+        elif model == "trip_mode_choice":
+            # assert one of the keys contains "trip_mode_choice"
+            assert any("trip_mode_choice" in key for key in skipped_household_ids_keys)
+            # assert that the number of skipped households for trip_mode_choice is 276
+            assert state.get("num_skipped_households", 0) == (69 + 598 + 276)
+            # assert no DRIVEALONEFREE tours in the remaining tours
+            assert all(state.get("tours").loc[:, "tour_mode"] != "DRIVEALONEFREE")
 
     # check that the number of skipped households is recorded in state
     assert state.get("num_skipped_households", 0) == 943
-
-    # check that there are no DRIVEALONEFREE tours in the final tours
-    final_tours_df = state.get_dataframe("tours")
-    assert "DRIVEALONEFREE" not in final_tours_df["tour_mode"].values
-
-    # check that there are no households in home zone 8 (recoded 7) in the final households
-    final_households_df = state.get_dataframe("households")
-    assert not any(final_households_df["home_zone_id"] == 7)
-
-    # check that there are no workers from households in home zone 18 in the final persons
-    final_persons_df = state.get_dataframe("persons")
-    assert not any(
-        (final_persons_df["home_zone_id"] == 18)
-        & (final_persons_df["is_worker"] == True)
-    )

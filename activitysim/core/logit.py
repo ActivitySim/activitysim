610ec1656002e7e38d2117f90d6febdf663d0a09
Jan Zill <jan@outerloop.io>
2026-07-31 15:13:15 +1000

    removes todo

--- activitysim/core/logit.py
+++ activitysim/core/logit.py
@@ -584,7 +584,9 @@ def make_choices_explicit_error_term_nl(
     pandas.Series
         Choice indices aligned to `alt_utilities` columns.
     """
-    # TODO assert alts_context and alt_nrs_df are both None - no sampling from nested models for now.
+    assert (
+        alts_context is None and alt_nrs_df is None
+    ), f"{trace_label} - Sampling from nested models is not implemented, do not pass alts_context or alt_nrs_df."
 
     utilities_incl_unobs = sample_nested_logit_exact_leaf_error_terms(
         state,

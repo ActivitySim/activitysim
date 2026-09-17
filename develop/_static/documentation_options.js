565a337ce64ad15dc01a58b70ec703121d5f98ee
janzill <janzill@users.noreply.github.com>
2026-09-17 01:08:23 +0000

    deploy: 8ca6741c83e44e70fbfe718cb91066dece989805

--- develop/_static/documentation_options.js
+++ develop/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.5.2.dev35+g5c6fae24a',
+    VERSION: '1.5.2.dev37+g8ca6741c8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
